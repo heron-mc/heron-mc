7675eeaadd1d3d20f8be657a9e7ad4e045f2993a
Just van den Broecke <just@justobjects.nl>
2010-09-21 15:49:42 +0000

    minor updates to default catalog and -config js

--- geoviewer/lib/DefaultConfig.js
+++ geoviewer/lib/DefaultConfig.js
@@ -182,7 +182,9 @@ GeoViewer.Map.layers = [
 	 * LKI Kadastrale Vlakken
 	 * ------------------------------ */
 	GeoViewer.Catalog.layers.lki_vlakken,
+	GeoViewer.Catalog.layers.lki_vlakken_tiled,
 	GeoViewer.Catalog.layers.lki_gebouwen,
+	GeoViewer.Catalog.layers.lki_gebouwen_tiled,
 	GeoViewer.Catalog.layers.lki_teksten,
 	GeoViewer.Catalog.layers.lki_perceelnrs,
 	GeoViewer.Catalog.layers.kadkaart,
