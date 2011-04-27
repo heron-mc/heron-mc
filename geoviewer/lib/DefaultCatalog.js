f5b482f7f49580d33c21549542597f29d1a91550
Just van den Broecke <just@justobjects.nl>
2011-04-27 16:03:40 +0000

    work on issue 28 (single file / enhanced config)

--- geoviewer/lib/DefaultCatalog.js
+++ geoviewer/lib/DefaultCatalog.js
@@ -44,12 +44,10 @@ GeoViewer.Map.options = {
 	PROJECTION: 'EPSG:28992',
 	UNITS: 'm',
 	RESOLUTIONS: [860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
-	MAX_EXTENT: new OpenLayers.Bounds(-65200.96, 242799.04, 375200.96, 683200.96),
-	TILE_ORIGIN: new OpenLayers.LonLat(-65200.96, 242799.04),
-	CENTER: new OpenLayers.LonLat(155000, 463000),
+	MAX_EXTENT: '-65200.96,242799.04,375200.96,683200.96',
+	CENTER: '155000,463000',
 	XY_PRECISION: 3,
-	ZOOM: 2,
-	MAX_EXTENT_KLIC1: new OpenLayers.Bounds(253865, 574237, 253960, 574727)
+	ZOOM: 2
 };
 
 
@@ -108,7 +106,7 @@ GeoViewer.Catalog.layers = {
 	blanco: new OpenLayers.Layer.Image(
 			"Blanco",
 			Ext.BLANK_IMAGE_URL,
-			GeoViewer.Map.options.MAX_EXTENT,
+			OpenLayers.Bounds.fromString(GeoViewer.Map.options.MAX_EXTENT),
 			new OpenLayers.Size(10, 10),
 	{resolutions: GeoViewer.Map.options.RESOLUTIONS, isBaseLayer: true, visibility: false, displayInLayerSwitcher: true}
 			),
