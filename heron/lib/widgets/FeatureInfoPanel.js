bb06e5eeef7e48a11393725e3688bc1a08237db1
Just van den Broecke <just@justobjects.nl>
2011-06-23 20:44:33 +0000

    issue 52: first version viewing complex data in FeatureInfoPanel

--- heron/lib/widgets/FeatureInfoPanel.js
+++ heron/lib/widgets/FeatureInfoPanel.js
@@ -13,6 +13,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 Ext.namespace("Heron.widgets");
+Ext.namespace("Heron.utils");
 
 /** api: (define)
  *  module = Heron.widgets
@@ -26,204 +27,285 @@ Ext.namespace("Heron.widgets");
  *  A tabbed panel designed to hold GetFeatureInfo for multiple layers
  */
 Heron.widgets.FeatureInfoPanel = Ext.extend(Ext.Panel, {
-	maxFeatures	: 5,
-	tabs		: null,
-	map		: null,
+			maxFeatures	: 5,
+			tabs		: null,
+			map		: null,
+			displayPanel : null,
+			lastEvt : null,
 
-	initComponent : function() {
-		Ext.apply(this, {
-			layout		: "fit",
-			title		: __('Feature info')
-		});
-
-		Heron.widgets.FeatureInfoPanel.superclass.initComponent.call(this);
-		this.map = Heron.App.getMap();
-
-		/***
-		 * Add a WMSGetFeatureInfo control to the map if it is not yet present
-		 */
-		var panel = this;
-
-		var control = null;
-
-		if (this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo").length > 0) {
-			control = this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo")[0];
-		}
-
-		if (control == undefined) {
-			control = new OpenLayers.Control.WMSGetFeatureInfo({
-				maxFeatures	: this.maxFeatures,
-				queryVisible: true,
-				infoFormat : "application/vnd.ogc.gml"
-			});
-
-			control.events.register("getfeatureinfo", this, this.handleGetFeatureInfo);
-			control.events.register("beforegetfeatureinfo", this, this.handleBeforeGetFeatureInfo);
-
-			// console.log(control);
-
-			this.map.addControl(control);
-		} else {
-			control.events.register("getfeatureinfo", this, this.handleGetFeatureInfo);
-			control.events.register("beforegetfeatureinfo", this, this.handleBeforeGetFeatureInfo);
-		}
-
-		this.on(
-				"render",
-				function() {
-					this.mask = new Ext.LoadMask(this.body, {msg:__('Loading...')})
-				});
-	},
-	handleBeforeGetFeatureInfo : function(evt) {
-		var control = this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo")[0];
-		control.layers = [];
-
-		var layer;
-		for (var index = 0; index < this.map.layers.length; index++) {
-			layer = this.map.layers[index];
-			if (layer.visibility && layer.featureInfoFormat) {
-				control.layers.push(layer);
-			}
-		}
-
-
-		if (control.layers.length == 0) {
-			alert(__('Feature Info unavailable'));
-			return;
-		}
-
-		this.expand();
-		if (this.tabs != undefined) {
-			this.tabs.removeAll();
-		}
-		this.mask.show();
-	},
-	/***
-	 * Callback function for handling the result of an OpenLayers GetFeatureInfo request
-	 */
-	handleGetFeatureInfo : function(evt) {
-		this.mask.hide();
-
-		var types = new Array();
-
-		for (var index = 0; index < evt.features.length; index++) {
-			var rec = evt.features[index];
-
-			var featureType = /[^\.]*/.exec(rec.fid);
-
-			featureType = (featureType[0] != "null") ? featureType[0] :
-					__('Unknown');
-
-			var found = false;
-			var type = null;
-
-			for (var j = 0; j < types.length; j++) {
-				type = types[j];
-
-				if (type.featureType == featureType) {
-					found = true;
-				}
-			}
-
-			if (!found) {
-				type = {
-					featureType : featureType,
-					columns		: new Array(),
-					fields		: new Array(),
-					records		: new Array()
-				};
-
-				types.push(type);
-			}
-
-			/***
-			 * GetFeatureInfo response can contain dots in the fieldnames, these are not allowed in ExtJS store fieldnames.
-			 *
-			 * Use a regex to replace the dots /w underscores.
-			 */
-			for (var attrib in rec.attributes) {
-				var new_attrib = attrib.replace(/\./g, "_");
-
-				rec.attributes[new_attrib] = rec.attributes[attrib];
-
-				if (attrib != new_attrib) {
-					delete rec.attributes[attrib];
-				}
-			}
-
-			// Populate columns and fields arrays
-			if (type.records.length == 0) {
-				for (var attrib in rec.attributes) {
-					if (type.records.length == 0) {
-						type.columns.push({
-							header : attrib,
-							width : 100,
-							dataIndex : attrib
+			initComponent : function() {
+				var self = this;
+				Ext.apply(this, {
+							layout		: "fit",
+							title		: __('Feature info'),
+							tbar: [
+								{
+									text: 'Grid',
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: true,
+									handler: function(t) {
+										self.display = self.displayGrid;
+										self.handleGetFeatureInfo();
+									}
+								},
+								{
+									text: 'Tree',
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: false,
+									handler: function(t) {
+										self.display = self.displayTree;
+										self.handleGetFeatureInfo();
+									}
+								},
+								{
+									text: 'XML',
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: false,
+									handler: function(t) {
+										self.display = self.displayXML;
+										self.handleGetFeatureInfo();
+									}
+								}
+							]
 						});
 
+				Heron.widgets.FeatureInfoPanel.superclass.initComponent.call(this);
+				this.map = Heron.App.getMap();
+				this.display = this.displayGrid;
 
-						type.fields.push(attrib);
+				/***
+				 * Add a WMSGetFeatureInfo control to the map if it is not yet present
+				 */
+				var control = null;
+
+				if (this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo").length > 0) {
+					control = this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo")[0];
+				}
+
+				if (control == undefined) {
+					control = new OpenLayers.Control.WMSGetFeatureInfo({
+								maxFeatures	: this.maxFeatures,
+								queryVisible: true,
+								infoFormat : "application/vnd.ogc.gml"
+							});
+
+					control.events.register("getfeatureinfo", this, this.handleGetFeatureInfo);
+					control.events.register("beforegetfeatureinfo", this, this.handleBeforeGetFeatureInfo);
+
+					// console.log(control);
+
+					this.map.addControl(control);
+				} else {
+					control.events.register("getfeatureinfo", this, this.handleGetFeatureInfo);
+					control.events.register("beforegetfeatureinfo", this, this.handleBeforeGetFeatureInfo);
+				}
+
+				this.on(
+						"render",
+						function() {
+							this.mask = new Ext.LoadMask(this.body, {msg:__('Loading...')})
+						});
+			},
+
+			handleBeforeGetFeatureInfo : function(evt) {
+				var control = this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo")[0];
+				control.layers = [];
+
+				var layer;
+				for (var index = 0; index < this.map.layers.length; index++) {
+					layer = this.map.layers[index];
+					if (layer.visibility && layer.featureInfoFormat) {
+						control.layers.push(layer);
 					}
 				}
-			}
 
 
-			type.records.push(rec.attributes);
-		}
+				if (control.layers.length == 0) {
+					alert(__('Feature Info unavailable'));
+					return;
+				}
 
-		while (types.length > 0) {
-			// TODO : Link typename to layer name			
-			type = types.pop();
+				this.lastEvt = null;
+				this.expand();
+				if (this.tabs != undefined) {
+					this.tabs.removeAll();
+				}
+				this.mask.show();
+			},
 
-			if (type.records.length > 0) {
-				var grid = new Ext.grid.GridPanel({
-					store : new Ext.data.JsonStore({
-						autoDestroy : true,
-						fields : type.fields,
-						data : type.records
-					}),
-					title : type.featureType,
-					colModel: new Ext.grid.ColumnModel({
-						defaults: {
-							width: 120,
-							sortable: true
-						},
-						columns : type.columns,
-						autoScroll : true,
-						listeners : {
-							"render" : function(c) {
-								c.doLayout();
+			handleGetFeatureInfo : function(evt) {
+				this.mask.hide();
+				if (evt) {
+					this.lastEvt = evt;
+				}
+
+				if (!this.lastEvt) {
+					return;
+				}
+
+				if (this.displayPanel) {
+					this.remove(this.displayPanel);
+				}
+
+				this.displayPanel = this.display(this.lastEvt);
+
+				if (this.displayPanel) {
+					this.add(this.displayPanel);
+					this.displayPanel.doLayout();
+				}
+
+				if (this.getLayout()) {
+					this.getLayout().runLayout();
+				}
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as grid)
+			 */
+			displayGrid : function(evt) {
+				var types = new Array();
+
+				for (var index = 0; index < evt.features.length; index++) {
+					var rec = evt.features[index];
+
+					var featureType = /[^\.]*/.exec(rec.fid);
+
+					featureType = (featureType[0] != "null") ? featureType[0] :
+							__('Unknown');
+
+					var found = false;
+					var type = null;
+
+					for (var j = 0; j < types.length; j++) {
+						type = types[j];
+
+						if (type.featureType == featureType) {
+							found = true;
+						}
+					}
+
+					if (!found) {
+						type = {
+							featureType : featureType,
+							columns		: new Array(),
+							fields		: new Array(),
+							records		: new Array()
+						};
+
+						types.push(type);
+					}
+
+					/***
+					 * GetFeatureInfo response can contain dots in the fieldnames, these are not allowed in ExtJS store fieldnames.
+					 *
+					 * Use a regex to replace the dots /w underscores.
+					 */
+					for (var attrib in rec.attributes) {
+						var new_attrib = attrib.replace(/\./g, "_");
+
+						rec.attributes[new_attrib] = rec.attributes[attrib];
+
+						if (attrib != new_attrib) {
+							delete rec.attributes[attrib];
+						}
+					}
+
+					// Populate columns and fields arrays
+					if (type.records.length == 0) {
+						for (var attrib in rec.attributes) {
+							if (type.records.length == 0) {
+								type.columns.push({
+											header : attrib,
+											width : 100,
+											dataIndex : attrib
+										});
+
+
+								type.fields.push(attrib);
 							}
 						}
-					})
-				});
+					}
 
-				if (this.tabs == null) {
-					this.tabs = this.add(new Ext.TabPanel({
-						border : false,
-						autoDestroy : true,
-						height : this.getHeight(),
-						items : [grid],
-						activeTab : 0
-					}));
-				} else {
-					this.tabs.add(grid);
 
-					this.tabs.setActiveTab(0);
+					type.records.push(rec.attributes);
 				}
+
+				if (this.tabs != null) {
+					this.remove(this.tabs);
+					this.tabs = null;
+				}
+
+				while (types.length > 0) {
+					// TODO : Link typename to layer name
+					type = types.pop();
+					if (type.records.length > 0) {
+						var grid = new Ext.grid.GridPanel({
+									store : new Ext.data.JsonStore({
+												autoDestroy : true,
+												fields : type.fields,
+												data : type.records
+											}),
+									title : type.featureType,
+									colModel: new Ext.grid.ColumnModel({
+												defaults: {
+													width: 120,
+													sortable: true
+												},
+												columns : type.columns,
+												autoScroll : true,
+												listeners : {
+													"render" : function(c) {
+														c.doLayout();
+													}
+												}
+											})
+								});
+
+						if (this.tabs == null) {
+							this.tabs = this.add(new Ext.TabPanel({
+										border : false,
+										autoDestroy : true,
+										height : this.getHeight(),
+										items : [grid],
+										activeTab : 0
+									}));
+						} else {
+							this.tabs.add(grid);
+
+							this.tabs.setActiveTab(0);
+						}
+					}
+				}
+				return this.tabs;
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as Tree)
+			 */
+			displayTree : function(evt) {
+				var panel = new Heron.widgets.XMLTreePanel();
+
+				panel.xmlTreeFromText(panel, evt.text);
+
+				return panel;
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as XML)
+			 */
+			displayXML : function(evt) {
+				var opts = {
+					html: '<div class="hr-html-panel-body"><pre>' + Heron.Utils.formatXml(evt.text, true) + '</pre></div>',
+					preventBodyReset: true,
+					autoScroll: true
+				};
+
+				return new Ext.Panel(opts);
 			}
-		}
-
-		if (this.tabs) {
-			this.tabs.doLayout();
-		}
-
-		if (this.getLayout()) {
-			this.getLayout().runLayout();
-		}
-	}
-});
+		});
 
 /** api: xtype = hr_featureinfopanel */
 Ext.reg('hr_featureinfopanel', Heron.widgets.FeatureInfoPanel);
-
