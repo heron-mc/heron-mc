85bb436c9ec05f8df4dc372ff64c93a0c422c6bd
justb4 <justb4@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2011-07-19 11:37:55 +0000

    "release-0.64"

---
+++ tags/release-0.64/heron/lib/i18n/en_US.js
@@ -0,0 +1,73 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (en_US)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the US locale.
+ * Maintained by: Heron devs
+ */
+Heron.i18n.dict = {
+	'Active Layers' : 'Active Layers',
+	'Base Layers': 'Base Layers',
+	'BaseMaps': 'Base Maps',
+	'Warning': 'Warning',
+	'Legend': 'Legend',
+	'No layer selected': 'No layer selected',
+	'Save Features': 'Save Features',
+	'Get Features': 'Get Features',
+	'Feature information': 'Feature information',
+	'No Information found': 'No Information found',
+	'Layer not added': 'Layer not added',
+	'Attribute': 'Attribute',
+	'Value': 'Value',
+	'Recieving data':'Recieving data',
+	'Layers': 'Layers',
+	'No match': 'No match',
+	'Loading...': 'Loading...',
+	'Shortcuts': 'Shortcuts',
+	'Places': 'Places',
+	'Feature Info': 'Feature Info',
+	'Feature Data': 'Feature Data',
+	'Feature(s)': 'feature(s)',
+	'Unknown': 'Unknown',
+	'Feature Info unavailable':'Feature Info unavailable',
+	'Pan': 'Pan',
+	'Measure length': 'Measure length',
+	'Measure area': 'Measure Area',
+	'Length': 'Length',
+	'Area': 'Area',
+	'Waiting for': 'Waiting for',
+	'Search': 'Search',
+	'Search Nominatim': 'Search (OSM) data by name and address',
+	'Search OpenLS' : 'Search with OpenLS service',
+	'Search PDOK': 'Type (parts of) Dutch national address',
+	'Searching...': 'Searching...',
+	'Search Completed: ': 'Search completed: ',
+	'services':'services',
+	'service':'service',
+	'Type Nominatim': 'Type a placename or address...',
+	'Base Layer': 'Base Layer',
+	'Zoom in': 'Zoom in',
+	'Zoom out': 'Zoom out',
+	'Zoom to full extent':'Zoom to full extent',
+	'Zoom previous': 'Zoom previous',
+	'Zoom next': 'Zoom next'
+};
---
+++ tags/release-0.64/heron/lib/widgets/FeatureInfoPanel.js
@@ -0,0 +1,328 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.widgets");
+Ext.namespace("Heron.utils");
+
+/** api: (define)
+ *  module = Heron.widgets
+ *  class = FeatureInfoPanel
+ *  base_link = `Ext.Panel <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.Panel>`_
+ */
+
+/** api: constructor
+ *  .. class:: FeatureInfoPanel(config)
+ *
+ *  A tabbed panel designed to hold GetFeatureInfo for multiple layers
+ */
+Heron.widgets.FeatureInfoPanel = Ext.extend(Ext.Panel, {
+			maxFeatures	: 5,
+			tabPanel : null,
+			map		: null,
+			displayPanel : null,
+			lastEvt : null,
+			olControl: null,
+
+			initComponent : function() {
+				// For closure ("this" is not valid in callbacks)
+				var self = this;
+
+				Ext.apply(this, {
+							layout		: "fit",
+							title		: __('Feature info'),
+							tbar: [
+								{
+									text: __('Grid'),
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: true,
+									handler: function(t) {
+										self.display = self.displayGrid;
+										self.handleGetFeatureInfo();
+									}
+								},
+								{
+									text: __('Tree'),
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: false,
+									handler: function(t) {
+										self.display = self.displayTree;
+										self.handleGetFeatureInfo();
+									}
+								},
+								{
+									text: __('XML'),
+									toggleGroup: "featInfoGroup",
+									enableToggle: true,
+									pressed: false,
+									handler: function(t) {
+										self.display = self.displayXML;
+										self.handleGetFeatureInfo();
+									}
+								}
+							]
+						});
+
+				Heron.widgets.FeatureInfoPanel.superclass.initComponent.call(this);
+				this.map = Heron.App.getMap();
+				this.display = this.displayGrid;
+
+				/***
+				 * Add a WMSGetFeatureInfo control to the map if it is not yet present
+				 */
+				var controls = this.map.getControlsByClass("OpenLayers.Control.WMSGetFeatureInfo");
+				if (controls && controls.length > 0) {
+					this.olControl = controls[0];
+				}
+
+				if (!this.olControl) {
+					this.olControl = new OpenLayers.Control.WMSGetFeatureInfo({
+								maxFeatures	: this.maxFeatures,
+								queryVisible: true,
+								infoFormat : "application/vnd.ogc.gml"
+							});
+
+					this.map.addControl(this.olControl);
+				}
+
+				this.olControl.events.register("getfeatureinfo", this, this.handleGetFeatureInfo);
+				this.olControl.events.register("beforegetfeatureinfo", this, this.handleBeforeGetFeatureInfo);
+
+				this.on(
+						"render",
+						function() {
+							this.mask = new Ext.LoadMask(this.body, {msg:__('Loading...')})
+						});
+			},
+
+			handleBeforeGetFeatureInfo : function(evt) {
+				this.olControl.layers = [];
+
+				// Needed to force accessing multiple WMS-es when multiple layers are visible
+				this.olControl.url = null;
+				this.olControl.drillDown = true;
+
+				// Select layers that are visible and enabled (via featureInfoFormat prop)
+				var layer;
+				for (var index = 0; index < this.map.layers.length; index++) {
+					layer = this.map.layers[index];
+					if (layer.visibility && layer.featureInfoFormat) {
+						this.olControl.layers.push(layer);
+					}
+				}
+
+				// TODO this really should be done by subscribing to the "nogetfeatureinfo"  event
+				// of OpenLayers.Control.WMSGetFeatureInfo
+				if (this.olControl.layers.length == 0) {
+					alert(__('Feature Info unavailable'));
+					return;
+				}
+
+				this.lastEvt = null;
+				this.expand();
+				if (this.tabPanel != undefined) {
+					this.tabPanel.removeAll();
+				}
+
+				// Show loading mask
+				this.mask.show();
+			},
+
+			handleGetFeatureInfo : function(evt) {
+				// Hide the loading mask
+				this.mask.hide();
+
+				// Save result e.g. when changing views
+				if (evt) {
+					this.lastEvt = evt;
+				}
+
+				if (!this.lastEvt) {
+					return;
+				}
+
+				if (this.displayPanel) {
+					this.remove(this.displayPanel);
+				}
+
+				// Delegate to current display panel (Grid, Tree, XML)
+				this.displayPanel = this.display(this.lastEvt);
+
+				if (this.displayPanel) {
+					this.add(this.displayPanel);
+					this.displayPanel.doLayout();
+				}
+
+				if (this.getLayout()) {
+					this.getLayout().runLayout();
+				}
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as grid)
+			 */
+			displayGrid : function(evt) {
+				var types = new Array();
+
+				for (var index = 0; index < evt.features.length; index++) {
+					var rec = evt.features[index];
+
+					// TODO: this is nasty and GeoServer specific ?
+					// We may check the FT e.g. from the GML tag(s) available in the evt
+                    // More specific, we need to. Because now with multiple layers, all are assigned to
+                    // unknown and you get strange column results when the featuretypes are mixed..
+					var featureType = /[^\.]*/.exec(rec.fid);
+
+					featureType = (featureType[0] != "null") ? featureType[0] :
+							__('Unknown');
+
+					var found = false;
+					var type = null;
+
+					for (var j = 0; j < types.length; j++) {
+						type = types[j];
+
+						if (type.featureType == featureType) {
+							found = true;
+						}
+					}
+
+					if (!found) {
+						type = {
+							featureType : featureType,
+							columns		: new Array(),
+							fields		: new Array(),
+							records		: new Array()
+						};
+
+						types.push(type);
+					}
+
+					/***
+					 * GetFeatureInfo response can contain dots in the fieldnames, these are not allowed in ExtJS store fieldnames.
+					 *
+					 * Use a regex to replace the dots /w underscores.
+					 */
+					for (var attrib in rec.attributes) {
+						var new_attrib = attrib.replace(/\./g, "_");
+
+						rec.attributes[new_attrib] = rec.attributes[attrib];
+
+						if (attrib != new_attrib) {
+							delete rec.attributes[attrib];
+						}
+					}
+
+					// Populate columns and fields arrays
+					if (type.records.length == 0) {
+						for (var attrib in rec.attributes) {
+							if (type.records.length == 0) {
+								type.columns.push({
+											header : attrib,
+											width : 100,
+											dataIndex : attrib
+										});
+
+
+								type.fields.push(attrib);
+							}
+						}
+					}
+
+
+					type.records.push(rec.attributes);
+				}
+
+				// Remove any existing panel
+				if (this.tabPanel != null) {
+					this.remove(this.tabPanel);
+					this.tabPanel = null;
+				}
+
+				// Run through FTs
+				while (types.length > 0) {
+					// TODO : Link typename to layer name
+					type = types.pop();
+					if (type.records.length > 0) {
+						// Create the table grid
+						var grid = new Ext.grid.GridPanel({
+									store : new Ext.data.JsonStore({
+												autoDestroy : true,
+												fields : type.fields,
+												data : type.records
+											}),
+									title : type.featureType,
+									colModel: new Ext.grid.ColumnModel({
+												defaults: {
+													width: 120,
+													sortable: true
+												},
+												columns : type.columns,
+												autoScroll : true,
+												listeners : {
+													"render" : function(c) {
+														c.doLayout();
+													}
+												}
+											})
+								});
+
+						// Create tab panel for the first FT and add additional tabs for each FT
+						if (this.tabPanel == null) {
+							this.tabPanel = new Ext.TabPanel({
+										border : false,
+										autoDestroy : true,
+										height : this.getHeight(),
+										items : [grid],
+										activeTab : 0
+									});
+						} else {
+							// Add to existing tab panel
+							this.tabPanel.add(grid);
+
+							this.tabPanel.setActiveTab(0);
+						}
+					}
+				}
+				return this.tabPanel;
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as Tree)
+			 */
+			displayTree : function(evt) {
+				var panel = new Heron.widgets.XMLTreePanel();
+
+				panel.xmlTreeFromText(panel, evt.text);
+
+				return panel;
+			},
+
+			/***
+			 * Callback function for handling the result of an OpenLayers GetFeatureInfo request (display as XML)
+			 */
+			displayXML : function(evt) {
+				var opts = {
+					html: '<div class="hr-html-panel-body"><pre>' + Heron.Utils.formatXml(evt.text, true) + '</pre></div>',
+					preventBodyReset: true,
+					autoScroll: true
+				};
+
+				return new Ext.Panel(opts);
+			}
+		});
+
+/** api: xtype = hr_featureinfopanel */
+Ext.reg('hr_featureinfopanel', Heron.widgets.FeatureInfoPanel);
