fc55437d982352ba4a2761a78c91c5af14326260
Just van den Broecke <just@justobjects.nl>
2011-09-05 13:44:52 +0000

    Heron: added Spanish translations thanks to Jose Luis Racero gilmoth@gmail.com

---
+++ heron/lib/i18n/es_ES.js
@@ -0,0 +1,88 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (en_US)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the US locale.
+ * Maintained by: Jose Luis Racero gilmoth@gmail.com
+ */
+Heron.i18n.dict = {
+	'Active Layers' : 'Capas Activas',
+	'Base Layers': 'Capas Base',
+	'BaseMaps': 'Mapas Base',
+	'Warning': 'Warning',
+	'Legend': 'Leyenda',
+	'No layer selected': 'Ninguna capa selecionada',
+	'Save Features': 'Guardar Forma',
+	'Get Features': 'Obtener Forma',
+	'Feature information': 'Informaci&oacute;n del elemento',
+	'No Information found': 'No se ha encontrado informaci&oacute;n',
+	'Layer not added': 'Capa no a&ntilde;adida',
+	'Attribute': 'Atributos',
+	'Value': 'Valor',
+	'Recieving data':'Reciciendo datos',
+	'Layers': 'Capas',
+	'No match': 'No match',
+	'Loading...': 'Cargando...',
+	'Shortcuts': 'Localizaciones', 
+	'Places': 'Lugares',
+	'Feature Info': 'Informaci&oacute;n del elemento ',
+	'Feature Data': 'Datos del elemento',
+	'Feature(s)': 'elementos(s)',
+	'Unknown': 'Desconocido',
+	'Feature Info unavailable':'Información del elemento no disponible',
+	'Pan': 'Mover mapa',
+	'Measure length': 'Medir distancia',
+	'Measure area': 'Medir Area',
+	'Length': 'Longitud',
+	'Area': 'Area',
+	'Waiting for': 'Esperando',
+	'Search': 'Busqueda',
+	'Search Nominatim': 'Busqueda (OSM) por nombre y direcci&oacute;n',
+	'Search OpenLS' : 'Buscar con servicio OpenLS',
+	'Search PDOK': 'Type (parts of) Dutch national address',
+	'Searching...': 'Buscando...',
+	'Search Completed: ': 'Busqueda completada: ',
+	'services':'servicios',
+	'service':'servicio',
+	'Type Nominatim': 'Escribe un lugar o direcci&oacute;n...',
+	'Base Layer': 'Capa Base',
+	'Zoom in': 'Acercar Zoom',
+	'Zoom out': 'Alejar Zoom',
+	'Zoom to full extent':'Zoom vista general',
+	'Zoom previous': 'Zoom anterior',
+	'Zoom next': 'Zoom siguiente'
+};
