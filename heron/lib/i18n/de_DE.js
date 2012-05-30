093234b306a83f9464d2e1ea65bdf2c8968956ba
Just van den Broecke <just@justobjects.nl>
2012-05-30 07:37:51 +0000

    German translations for Heron, thanks to Michael Räder

---
+++ heron/lib/i18n/de_DE.js
@@ -0,0 +1,76 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (en_US)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the US locale.
+ * Maintained by: Heron devs
+ */
+Heron.i18n.dict = {
+	'Active Layers' : 'Aktive Layer',
+	'Base Layers': 'Basis Layer',
+	'Base Layer': 'Basis Layer',
+	'BaseMaps': 'Basis Maps',
+	'Warning': 'Warnung',
+	'Legend': 'Legende',
+	'No layer selected': 'Kein Layer ausgew&#228;hlt',
+	'Save Features': 'Speichere Features',
+	'Get Features': 'Zeige Features',
+	'Feature information': 'Feature Information',
+	'No Information found': 'Keine Information gefunden',
+	'Layer not added': 'Kein Layer hinzugefügt',
+	'Attribute': 'Attribut',
+	'Value': 'Wert',
+	'Recieving data':'Erhalte Daten',
+	'Layers': 'Layer',
+	'No match': 'Keine &#220;bereinstimmung',
+	'Loading...': 'Laden...',
+	'Shortcuts': 'Lesezeichen',
+	'Places': 'Orte',
+	'Feature Info': 'Feature Info',
+	'Feature Data': 'Feature Data',
+	'Feature(s)': 'feature(s)',
+	'Unknown': 'Unbekannt',
+	'Feature Info unavailable':'Feature Info nicht verfügbar',
+	'Pan': 'Verschieben',
+	'Measure length': 'L&#228;nge messen',
+	'Measure area': 'Fl&#228;che messen',
+	'Length': 'L&#228;nge',
+	'Area': 'Fl&#228;che',
+	'Result >' : 'Resultat >',
+	'Search': 'Suche',
+	'Search Nominatim': 'Suche (OSM) data mit Namen und Adresse',
+	'Search OpenLS' : 'Suche mit OpenLS service',
+	'Search PDOK': 'Eingabe (parts of) deutsche nationale Addresse',
+	'Searching...': 'Suche...',
+	'Search Completed: ': 'Suche abgeschlossen: ',
+	'services':'Services',
+	'service':'Service',
+	'Type Nominatim': 'Eingabe Ort oder Adresse...',
+	'Waiting for': 'Warte auf',
+	'Zoom in': 'Hereinzoomen',
+	'Zoom out': 'Herauszoomen',
+	'Zoom to full extent':'Zoom auf max. Ausdehnung',
+	'Zoom previous': 'vorherige Zoomstufe',
+	'Zoom next': 'n&#228;chste Zoomstufe',
+	'< Search' : '< Suche'
+};
