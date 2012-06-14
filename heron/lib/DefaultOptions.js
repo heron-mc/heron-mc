65b909034e654fc1c39e273257c611fd478a20bc
Just van den Broecke <just@justobjects.nl>
2012-06-14 08:18:29 +0000

    some further work on issue #15 - printing facilities - to be finalized ...

--- heron/lib/DefaultOptions.js
+++ heron/lib/DefaultOptions.js
@@ -35,16 +35,51 @@ var icon = new OpenLayers.Icon("/media/logo_kadasterK_klein.GIF", size, null, ca
  *
  **/
 Ext.namespace("Heron.options.map");
+Ext.namespace("Heron.pdok");
+
+/** Standard tiling Netherlands: upperleft: X=-285.401,920 Y=903.401,920;
+ * lowerright: X=595.401,920 Y=22.598,080;
+ * lowerleft: X=-285.401,920  Y=22.598,080;
+ * This results in:
+ * maxExtent: '-285401.920,22598.080,595401.920,903401.920'
+ * on zoomLevel 2 more common for NL:
+ * maxExtent: '-65200.96,242799.04,375200.96,683200.96',
+ * but when using TMS all levels needed
+*/
 
 Heron.options.map.settings = {
 	projection: 'EPSG:28992',
 	units: 'm',
-	resolutions: [860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
-	maxExtent: '-65200.96,242799.04,375200.96,683200.96',
+
+	resolutions: [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
+	maxExtent: '-285401.920, 22598.080, 595401.920, 903401.920',
+/*	resolutions: [860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],      */
+/*	maxExtent: '-65200.96,242799.04,375200.96,683200.96', */
 	center: '155000,463000',
 	xy_precision: 3,
 	zoom: 2,
 	theme: null
+};
+
+Heron.PDOK = {
+	baseURL: 'http://geodata.nationaalgeoregister.nl',
+	TMS: 'http://geodata.nationaalgeoregister.nl/tms/',
+	WMTS:  'http://geodata.nationaalgeoregister.nl/tiles/service/wmts',
+	tileOriginLL: new OpenLayers.LonLat(-285401.920, 22598.080),
+	tileOriginUL: new OpenLayers.LonLat(-285401.920, 903401.920),
+	tileFullExtent:	new OpenLayers.Bounds(-285401.920, 22598.080, 595401.920, 903401.920),
+	serverResolutions : [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210, 0.105, 0.0525],
+	matrixIds : new Array(15),
+	zoomOffset: 2
+};
+
+// For WMTS layers in EPSG:28992
+for (var i=0; i < 15; ++i) {
+    Heron.PDOK.matrixIds[i] = "EPSG:28992:" + i;
+}
+
+Heron.PDOK.urls ={
+	NATURA2000_WMTS:  Heron.PDOK.WMTS + '/natura2000?'
 };
 
 Heron.scratch.urls = {
@@ -65,6 +100,30 @@ Heron.scratch.layermap = {
 	 * ==================================
 	 */
 
+	pdok_brtachtergrondkaart: new OpenLayers.Layer.TMS("BRT Achtergrondkaart",
+				Heron.PDOK.TMS,
+				{layername: 'brtachtergrondkaart',
+					type: "png",
+					isBaseLayer: true,
+					transparent: true,
+					bgcolor: "0xffffff",
+					visibility: true,
+					singleTile: false,
+					alpha:true, opacity: 1.0,
+					attribution: "Bron: BRT Achtergrondkaart, © <a href='http://openstreetmap.org/'>OpenStreetMap</a> <a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-By-SA</a>"}),
+
+	pdok_natura2000_wmts: new OpenLayers.Layer.WMTS({
+		name: "Natura 2000 (WMTS)",
+		url: Heron.PDOK.urls.NATURA2000_WMTS,
+		layer: "natura2000",
+		matrixSet: "EPSG:28992",
+		matrixIds: Heron.PDOK.matrixIds,
+		format: "image/png",
+		visibility: false,
+		style: "_null",
+		opacity: 0.7,
+		isBaseLayer: false
+	}),
 
 	/*
 	 * Basemap openStreetMap TileCache+Mapnik
@@ -363,7 +422,8 @@ Heron.options.map.layers = [
 	 *            BaseLayers
 	 * ==================================
 	 */
-	Heron.scratch.layermap.osm,
+	Heron.scratch.layermap.pdok_natura2000_wmts,
+	Heron.scratch.layermap.pdok_brtachtergrondkaart,
 	Heron.scratch.layermap.topraster,
 	Heron.scratch.layermap.top10nlgeodan,
 	Heron.scratch.layermap.luchtfotonlr,
