adfef7e72dab6f1e4fdab70259569d68966966e1
justb4 <justb4@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2012-08-14 14:36:24 +0000

    Fix issue #82:	Make text in grid for GetFeatureInfoPanel selectable

---
+++ trunk/heron/lib/override-ext.js
@@ -0,0 +1,30 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * To make grid cell text by default selectable, see http://www.sencha.com/learn/grid-faq/
+ * See also default.css for GridView cell Template CSS definition for "x-selectable".
+ * Issue: http://code.google.com/p/geoext-viewer/issues/detail?id=82
+ */
+if (!Ext.grid.GridView.prototype.templates) {
+   Ext.grid.GridView.prototype.templates = {};
+}
+Ext.grid.GridView.prototype.templates.cell = new Ext.Template(
+   '<td class="x-grid3-col x-grid3-cell x-grid3-td-{id} x-selectable {css}" style="{style}" tabIndex="0" {cellAttr}>',
+   '<div class="x-grid3-cell-inner x-grid3-col-{id}" {attr}>{value}</div>',
+   '</td>'
+);
+
+
