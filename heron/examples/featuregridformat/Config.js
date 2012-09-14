305fb8e517ab3db631ed2c2f339a90fe9a3d01dc
Just van den Broecke <just@justobjects.nl>
2012-09-14 15:41:20 +0000

    minor editorial changes for issue #76 (test different config)

--- heron/examples/featuregridformat/Config.js
+++ heron/examples/featuregridformat/Config.js
@@ -12,6 +12,21 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 Ext.namespace("Heron");
 
 OpenLayers.Util.onImageLoadErrorColor = "transparent";
@@ -111,12 +126,12 @@ Heron.layout = {
 								fn : Heron.widgets.GridCellRenderer.browserPopupLink,
 								options : {
 									url: 'http://en.wikipedia.org/wiki/{Country}',
-									winName: 'demoWin',
+									winName: 'hrdemoWin',
 									bReopen: false,
-									hasMenubar: true,
-									hasToolbar: true,
-									hasAddressbar: true,
-									hasStatusbar: true,
+									hasMenubar: false,
+									hasToolbar: false,
+									hasAddressbar: false,
+									hasStatusbar: false,
 									hasScrollbars: true,
 									isResizable: true,
 									hasPos: true,
