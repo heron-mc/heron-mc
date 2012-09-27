93d9868576504e065d03efc593720c9cd406411b
Just van den Broecke <just@justobjects.nl>
2012-09-27 12:04:20 +0000

    Czech i18N thanks to Martin Kokeš

---
+++ heron/lib/i18n/cs_CZ.js
@@ -0,0 +1,107 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (de_DE)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the DE locale.
+ * Maintained by: Heron devs
+ */
+Heron.i18n.dict = {
+    // 0.67
+	'Active Layers' : 'Aktivní vrstvy',
+	'Base Layer': 'Základní vrstva',
+	'Base Layers': 'Základní vrstvy',
+	'BaseMaps': 'Základní mapy',
+	'Choose a Base Layer': 'Vyberte základní vrstvu',
+	'Legend': 'Legenda',
+	'Feature Info': 'Informace o prvku',
+	'Feature Data': 'Data prvku',
+	'Feature(s)': 'Prvky',
+	'No layer selected': 'Nebyla vybrána žádná vrstva',
+	'Save Features': 'Uložit prvky',
+	'Get Features': 'Získat prvky',
+	'Feature information': 'Informace o prvku',
+	'No Information found': 'Nenalezeny žádné informace',
+	'Layer not added': 'Vrstva nebyla přidána',
+	'Attribute': 'Atribut',
+	'Value': 'Hodnota',
+	'Recieving data':'Přijímám data',
+	'Layers': 'Vrstvy',
+	'No match': 'Žádná shoda',
+	'Loading...': 'Načítání...',
+	'Shortcuts': 'Záložky',
+	'Places': 'Místa',
+	'Unknown': 'Neznámý',
+	'Feature Info unavailable':'Informace o prvku nejsou k dispozici',
+	'Pan': '<b>Posuv</b><br>Držení levého tlačítka myši nad mapou posouvá aktuální<br>pohled; kolečko myši a pozice kurzoru nad mapou<br>zároveň ovládá přiblížení (i v jiných režimech než Posuv)',
+	'Measure length': '<b>Měření délky</b><br>Každé klepnutí na levé tlačítko myši nad mapou vytvoří<br>bod měřeného úseku, dvojité klepnutí měření ukončí',
+	'Measure area': '<b>Měření plochy</b><br>Každé klepnutí na levé tlačítko myši nad mapou vytvoří<br>bod měřeného polygonu, dvojité klepnutí měření ukončí',
+	'Leg' : 'Úsek',
+	'Length': 'Délka',
+	'Area': 'Plocha',
+	'Result >' : 'Výsledek >',
+	'< Search' : '< Hledat',
+	'Search': 'Hledat',
+	'Search Nominatim': 'Hledat (pomocí OSM Nominatim) podle názvu a adresy',
+	'Search OpenLS' : 'Hledat pomocí služby OpenLS',
+	'Search PDOK': 'Vložit (část) české národní adresy',
+	'Searching...': 'Hledání...',
+	'Search Completed: ': 'Hledání dokončeno: ',
+	'services':'služby',
+	'service':'službu',
+	'Type Nominatim': 'Zadejte název místa nebo adresu...',
+	'Overlays': 'Překryvné vrstvy',
+	'Waiting for': 'Čekám na',
+	'Warning': 'Varování',
+	'Zoom in': '<b>Přiblížit</b><br>Po klepnutí na levé tlačítko myši na mapě přiblíží pohled,<br>držením tlačítka a táhnutím lze označit plochu pro přiblížení',
+	'Zoom out': '<b>Oddálit</b><br>Po klepnutí na levé tlačítko myši na mapě oddálí pohled',
+	'Zoom to full extent':'<b>Oddálit na plný rozsah</b><br>Oddálí pohled pro plné zobrazení povoleného rozsahu mapy',
+	'Zoom previous': '<b>Předchozí pohled</b><br>Přejde na předchozí pohled (zvětšení i rozsah)',
+	'Zoom next': '<b>Další pohled</b><br>Přejde na další pohled (zvětšení i rozsah)',
+
+    // 0.xx
+	
+	'Scale': 'Měřítko',
+	'Resolution': 'Rozlišení',
+	'Zoom': 'Přiblížení',
+	
+	// r458
+	'Create PDF' : 'Vytvořit PDF',
+	'Print': 'Tisk',
+	'Print Dialog Popup': 'Dialog tisku',
+	'Print Visible Map Area' : 'Tisk viditelné plochy mapy'
+	
+};
