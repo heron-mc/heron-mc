d2a250540c999ed9846f8d783106976663466a0f
wolfram.winter@gmail.com <wolfram.winter@gmail.com@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2012-10-25 12:57:25 +0000

    issues #125 + #126 and other ActiveLayersPanel defects - solved

--- trunk/heron/lib/widgets/ActiveLayersPanel.js
+++ trunk/heron/lib/widgets/ActiveLayersPanel.js
@@ -28,18 +28,44 @@ Heron.widgets.ActiveLayerNode = Ext.extend(GeoExt.tree.LayerNode, {
 			var layer = this.layer instanceof OpenLayers.Layer && this.layer;
 		    if (layer && this.attributes && this.attributes.component && this.attributes.component.xtype == "gx_opacityslider") {
 				this.attributes.component.layer = layer;
-				this.attributes.component.value = layer.opacity ? (parseInt(layer.opacity * 100)) : 100;
+				// OL
+				if (layer.opacity>=1.0) {
+					layer.setOpacity(1.0);
+				}
+				else if (layer.opacity<0.0) {
+					layer.setOpacity(0.0);
+				}
+				// Slider
+				this.attributes.component.value = parseInt(layer.opacity * 100);
 			}
 
-			// call base class
+			// Call base class
 			Heron.widgets.ActiveLayerNode.superclass.render.call(this, bulkRender);
+
 			if (layer && this.attributes && this.attributes.component && this.attributes.component.xtype == "gx_opacityslider") {
-				if (layer.opacity) {
-					// Triggers opacity change event (only if new opacity differs from current)
-					// in order to force slider to right position
-					// See issue #65
-					layer.setOpacity(layer.opacity-0.001);
+				// Triggers opacity change event in order to force slider to right position
+				// See issue #65
+				// OL
+				if (layer.opacity>=1.0) {
+					layer.setOpacity(0.999);
+					layer.setOpacity(1.0);
 				}
+				else if (layer.opacity>=0.001) {
+					layer.setOpacity(layer.opacity-0.001);
+					layer.setOpacity(layer.opacity+0.001);
+				} else {
+					layer.setOpacity(0.001);
+					layer.setOpacity(0.0);
+				}
+				// Slider
+				this.attributes.component.value = parseInt(layer.opacity * 100);
+
+				// - WW -
+				// This doesn't work for a 'hr_activelayerspanel' component located in a NOT
+				// EXPANDED ExtJS panel item. After activating/expanding the panel the slider
+				// must be updated to the (new) slider value.
+				// => listeners method for 'activate' and 'expand' fixes this issue - see below.
+
 			}
 		}
 	}
@@ -91,6 +117,11 @@ Ext.tree.TreePanel.nodeTypes.hr_activelayer = Heron.widgets.ActiveLayerNode;
  */
 Heron.widgets.ActiveLayersPanel = Ext.extend(Ext.tree.TreePanel, {
 
+    /** api: config[title]
+     *  default value is "Active Themes".
+     */
+	title : __('Active Themes'),
+
 	applyStandardNodeOpts: function(opts, layer) {
 		if (opts.component) {
 			opts.component.layer = layer;
@@ -100,11 +131,10 @@ Heron.widgets.ActiveLayersPanel = Ext.extend(Ext.tree.TreePanel, {
 
 	initComponent : function() {
 		var self = this;
-
 		var options = {
 			// id: "hr-activelayers",
 			border: true,
-			title : __('Active Layers'),
+			title : this.title,
 			// collapseMode: "mini",
 			autoScroll: true,
 			enableDD: true,
@@ -141,6 +171,8 @@ Heron.widgets.ActiveLayersPanel = Ext.extend(Ext.tree.TreePanel, {
 
 		// Delay processing, since the Map and Layers may not be available.
 		this.addListener("afterrender", this.onAfterRender);
+		this.addListener("beforedblclick", this.onBeforeDblClick);
+		this.addListener("beforenodedrop", this.onBeforeNodeDrop);
 	},
 
 	createNode : function(self, attr) {
@@ -153,8 +185,45 @@ Heron.widgets.ActiveLayersPanel = Ext.extend(Ext.tree.TreePanel, {
 		attr.uiProvider = ActiveLayerNodeUI;
 		attr.nodeType = "hr_activelayer";
 		attr.iconCls = 'gx-activelayer-drag-icon';
-
 		return GeoExt.tree.LayerLoader.prototype.createNode.call(self, attr);
+	},
+
+	onBeforeDblClick : function(node, evt) {
+		// @event beforedblclick
+		// Fires before double click processing. Return false to cancel the default action.
+		// @param {Node} this This node
+		// @param {Ext.EventObject} e The event object
+		return false;
+	},
+
+	onBeforeNodeDrop : function(dropEvt) {
+		// @event beforenodedrop
+		// @param {dropEvt} drop event
+		// dropEvt properties:
+		// tree - The TreePanel
+		// target - The node being targeted for the drop
+		// data - The drag data from the drag source
+		// point - The point of the drop - append, above or below
+		// source - The drag source
+		// rawEvent - Raw mouse event
+		// dropNode - Drop node(s) provided by the source OR you can supply node(s) to be inserted by setting them on this object.
+		// cancel - Set this to true to cancel the drop.
+		// dropStatus - If the default drop action is cancelled but the drop is valid, setting this to true will prevent the animated 'repair' from appearing.
+		if (dropEvt) {
+			switch(dropEvt.point) {
+				case "above":
+					return true;
+					break;
+				case "below":
+					// No drop below a baselayer node
+					var layer = dropEvt.target.layer;
+					if (!layer.isBaseLayer) {
+						return true;
+					}
+					break;
+			}
+		}
+		return false;
 	},
 
 	onAfterRender : function() {
@@ -162,58 +231,119 @@ Heron.widgets.ActiveLayersPanel = Ext.extend(Ext.tree.TreePanel, {
 		var map = Heron.App.getMap();
 
 		map.events.register('changelayer', null, function(evt) {
+
 			var layer = evt.layer;
 			var rootNode = self.getRootNode();
-
 			var layerNode = rootNode.findChild('layerId', evt.layer.id);
 
 			if (evt.property === "visibility") {
-				// Quickfix issue 47: opacity should not be remembered
 
-				// Add or remove layer node dependent on visibility
+				// Add layer node dependent on visibility and if not in tree
 				if (evt.layer.getVisibility() && !layerNode) {
-					// Layer made visible: add if not yet in tree
-					var newNode = self.createNode(self, {
-						layer : layer
-					});
 
-					// Remember current top layer node in stack before adding new node
-					var topLayer;
-					if (rootNode.firstChild) {
-						topLayer = rootNode.firstChild.layer;
-					}
-					var bottomLayer;
-					if (rootNode.lastChild) {
-						bottomLayer = rootNode.lastChild.layer;
-					}
+					// Create new layer node
+					var newNode = self.createNode(self, {layer: layer});
+					var newLayerId = layer.map.getLayerIndex(layer);
 
-					// Always insert new Node as first child, i.e. on top of the layer stack
-					rootNode.insertBefore(newNode, rootNode.firstChild);
-
-					// Correct stack order
+ 					// Add layer node
 					if (layer.isBaseLayer) {
-						// WW : New baselayers are always on the bottom of the stack
-						// Fixes issue #114
-						if (map.getLayerIndex(bottomLayer) > 0) {
- 							map.setLayerIndex(newNode.layer, -1);
+						// baselayer
+ 						// Remember current bottom layer node in stack before adding new node
+						var bottomLayer;
+						var bottomLayerId;
+						if (rootNode.lastChild) {
+							bottomLayer = rootNode.lastChild.layer;
+							if (bottomLayer) {
+	 							bottomLayerId = bottomLayer.map.getLayerIndex(bottomLayer);
+							}
+						}
+						// Always insert as last child, i.e. to bottom of the layer stack
+						rootNode.appendChild(newNode);
+						// OL - layer index correction - push to bottom of the layer stack
+						if (bottomLayer) {
+							if (newLayerId > bottomLayerId) {
+								layer.map.raiseLayer(layer, bottomLayerId - newLayerId);
+							}
 						}
 					} else {
-						// JvdB : Always raise the new layer to become top layer by
-						// // raising its layer index higher than the current top layer
-						// Fixes issue #49 (stacking order incorrect sometimes)
-						if (topLayer) {
-							map.setLayerIndex(newNode.layer, map.getLayerIndex(topLayer) + 1);
+						// layer
+ 						// Remember current top layer node in stack before adding new node
+						var topLayer;
+						var topLayerId;
+						if (rootNode.firstChild) {
+							topLayer = rootNode.firstChild.layer;
+							if (topLayer) {
+	 							topLayerId = topLayer.map.getLayerIndex(topLayer);
+							}
 						}
+						// Always insert new node as first child, i.e. on top of the layer stack
+						rootNode.insertBefore(newNode, rootNode.firstChild);
+						// OL - layer index correction - push on top of the layer stack
+						if (topLayer) {
+							if (topLayerId > newLayerId ) {
+								layer.map.raiseLayer(layer, topLayerId - newLayerId);
+							}
+						}
+
 					}
 
 				} else if (!evt.layer.getVisibility() && layerNode) {
 					layerNode.un("move", self.onChildMove, self);
 					layerNode.remove();
-					// layer.setOpacity(1.0);
 				}
 			}
 		});
+	},
+
+	onListenerDoLayout : function (node) {
+		if (node && node.hropts && node.hropts.component && node.hropts.component.xtype == "gx_opacityslider") {
+			var rootNode = node.getRootNode();
+			// Set all LayerOpacitySlider values in this tree
+			rootNode.cascade( function(n) {
+				if (n.layer) {
+					n.component.setValue(parseInt(n.layer.opacity * 100));
+					n.component.syncThumb();
+
+					// - WW -
+					// If the 'ActiveLayersPanel' widget is located inside an ExtJs panel there
+					// is an gui error between GeoExt and ExtJs concerning the slider thumb.
+					// Case: if the 'gx_opacityslider' component is rendered, when its above node
+					// IS NOT EXPANDED (not visible) the thumb gui position is shifted a little
+					// bit to the right, so that the left end position of the slider could not be
+					// reached. The related slider values are correct - there is no functional
+					// limitation. If the above node IS EXPANDED the thumb of the 'gx_opacityslider'
+					// component is displayed at the correct place.
+					// Demo for the thumb (#) of the slider component (----) in the 0%/100% position:
+					//    - gui expected for 0%:	#----------
+					//    - gui display  for 0%:	-#---------
+					//    - gui expected for 100%:	----------#
+					//    - gui display  for 100%:	-----------#
+
+				}
+			} );
+			node.doLayout();
+		}
+	},
+
+    // method[listeners]
+    //  Force the 'gx_opacityslider' component to become visible ...
+    //  ... inside an activated TabPanel or inside an expanded (accordion) panel
+    //
+    //  ATTENTION
+    //  ---------
+    //  This listener only gets the events of the panel in which it is located - if there is a
+    //  further panel - higher above in the tree - you must define an additional / other listener
+    //  function to support the redraw of the slider component!!!
+    //
+	listeners: {
+		activate: function(node) {
+			this.onListenerDoLayout(this);
+		},
+		expand: function(node) {
+			this.onListenerDoLayout(this);
+		}
 	}
+
 });
 
 /** api: xtype = hr_activelayerspanel */
