1933e5dac10a83cfe58ebe93309bb253874618b3
wolfram.winter@gmail.com <wolfram.winter@gmail.com@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2012-11-05 11:46:45 +0000

    issues #133, #134, #135 Defects in LayerTreePanel, ActiveLayersPanel, ActiveThemesPanel - solved

--- trunk/heron/lib/override-geoext.js
+++ trunk/heron/lib/override-geoext.js
@@ -91,3 +91,119 @@ Ext.override(GeoExt.WMSLegend, {
     }
 });
 
+/*  NOTE (WW): override the GeoExt.tree.LayerNodeUI.enforceOneVisible() method:
+ *  to prevent null pointer assignment in 'this.node.getOwnerTree().getChecked()' when
+ *  using the 'hr_layertreepanel' panel without the 'hropts' option
+ *
+ *  Version below taken from GeoExt GitHub on 01.nov.2012
+ */
+Ext.override(GeoExt.tree.LayerNodeUI, {
+
+    /** private: method[enforceOneVisible]
+     *
+     *  Makes sure that only one layer is visible if checkedGroup is set.
+     */
+    enforceOneVisible: function() {
+        var attributes = this.node.attributes;
+        var group = attributes.checkedGroup;
+        // If we are in the baselayer group, the map will take care of
+        // enforcing visibility.
+        if(group && group !== "gx_baselayer") {
+            var layer = this.node.layer;
+// --- WW ---
+			if (attributes.checked) {
+// ----------
+            var checkedNodes = this.node.getOwnerTree().getChecked();
+            var checkedCount = 0;
+            // enforce "not more than one visible"
+            Ext.each(checkedNodes, function(n){
+                var l = n.layer;
+                if(!n.hidden && n.attributes.checkedGroup === group) {
+                    checkedCount++;
+                    if(l != layer && attributes.checked) {
+                        l.setVisibility(false);
+                    }
+                }
+            });
+            // enforce "at least one visible"
+            if(checkedCount === 0 && attributes.checked == false) {
+                layer.setVisibility(true);
+            }
+// ----------
+			}
+// ----------
+        }
+    }
+
+});
+
+/*  NOTE (WW): override the GeoExt.tree.LayerNode.render() method:
+ *  to prevent problems with the 'checkedGroup' flag for creating radiobuttons when
+ *  using the 'hr_activelayerspanel' or 'hr_activethemespanel' panel - instead of a
+ *  'baselayer radiobutton' a 'disabled baselayer checkbox' is shown
+ *
+ *  Version below taken from GeoExt GitHub on 01.nov.2012
+ */
+Ext.override(GeoExt.tree.LayerNode, {
+
+    /** private: method[render]
+     *  :param bulkRender: ``Boolean``
+     */
+    render: function(bulkRender) {
+        var layer = this.layer instanceof OpenLayers.Layer && this.layer;
+        if(!layer) {
+            // guess the store if not provided
+            if(!this.layerStore || this.layerStore == "auto") {
+                this.layerStore = GeoExt.MapPanel.guess().layers;
+            }
+            // now we try to find the layer by its name in the layer store
+            var i = this.layerStore.findBy(function(o) {
+                return o.get("title") == this.layer;
+            }, this);
+            if(i != -1) {
+                // if we found the layer, we can assign it and everything
+                // will be fine
+                layer = this.layerStore.getAt(i).getLayer();
+            }
+        }
+        if (!this.rendered || !layer) {
+            var ui = this.getUI();
+
+            if(layer) {
+                this.layer = layer;
+                // no DD and radio buttons for base layers
+                if(layer.isBaseLayer) {
+					this.draggable = false;
+// --- WW ---
+					// Don't use 'checkedGroup' argument
+
+					// Ext.applyIf(this.attributes, {
+					// checkedGroup: "gx_baselayer"
+					// });
+
+					// Disabled baselayer checkbox
+					this.disabled = true;
+// ----------
+                }
+
+                //base layers & alwaysInRange layers should never be auto-disabled
+                this.autoDisable = !(this.autoDisable===false || this.layer.isBaseLayer || this.layer.alwaysInRange);
+
+                if(!this.text) {
+                    this.text = layer.name;
+                }
+
+                ui.show();
+                this.addVisibilityEventHandlers();
+            } else {
+                ui.hide();
+            }
+
+            if(this.layerStore instanceof GeoExt.data.LayerStore) {
+                this.addStoreEventHandlers(layer);
+            }
+        }
+        GeoExt.tree.LayerNode.superclass.render.apply(this, arguments);
+    }
+
+});
