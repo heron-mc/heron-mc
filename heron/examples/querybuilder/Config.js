e2ae2d7a4e8af7524a633be5114a708f9b2879d0
Just van den Broecke <just@justobjects.nl>
2013-05-28 13:51:36 +0000

    issue 214 - GetFeatureInfo popup now working again on all new Search examples

--- heron/examples/querybuilder/Config.js
+++ heron/examples/querybuilder/Config.js
@@ -33,7 +33,21 @@ Ext.namespace("Heron.examples");
 // used. There you need only pass the options, similar as in the function
 // ExtJS Toolbar.add().
 Heron.options.map.toolbar = [
-    {type: "featureinfo", options: {max_features: 20}},
+    {type: "featureinfo", options: {
+        popupWindow: {
+            width: 360,
+            height: 200,
+            featureInfoPanel: {
+                // Option values are 'Grid', 'Tree' and 'XML', default is 'Grid' (results in no display menu)
+                displayPanels: ['Grid', 'XML', 'Tree'],
+                // Export to download file. Option values are 'CSV', 'XLS', default is no export (results in no export menu).
+                exportFormats: ['CSV', 'XLS'],
+                // Export to download file. Option values are 'CSV', 'XLS', default is no export (results in no export menu).
+                // exportFormats: ['CSV', 'XLS'],
+                maxFeatures: 10
+            }
+        }
+    }},
     {type: "-"} ,
     {type: "pan"},
     {type: "zoomin"},
