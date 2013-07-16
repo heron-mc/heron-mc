ad3cf3480be4466284d796612e3a006a7a3dbf8f
Just van den Broecke <just@justobjects.nl>
2013-07-16 09:05:33 +0000

    issue 250: Ext JS and OpenLayers hosted via CDNJS

--- heron/examples/DefaultOptionsNL.js
+++ heron/examples/DefaultOptionsNL.js
@@ -22,7 +22,7 @@ OpenLayers.Util.onImageLoadErrorColor = "transparent";
 OpenLayers.ProxyHost = "/cgi-bin/proxy.cgi?url=";
 OpenLayers.DOTS_PER_INCH = 25.4 / 0.28;
 
-Ext.BLANK_IMAGE_URL = 'http://lib.heron-mc.org/ext/3.4.1.1/resources/images/default/s.gif';
+Ext.BLANK_IMAGE_URL = 'http://cdnjs.cloudflare.com/ajax/libs/extjs/3.4.1-1/resources/images/default/s.gif';
 
 /**
  * Options for MapPanel
