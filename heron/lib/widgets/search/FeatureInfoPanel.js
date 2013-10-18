2e8ee4d8b890463ab363871f9bcbb121271d578e
wolfram.winter@gmail.com <wolfram.winter@gmail.com@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2013-10-18 07:39:33 +0000

    FeatureInfoPanel - issue 282 - fix

--- heron/lib/widgets/search/FeatureInfoPanel.js
+++ heron/lib/widgets/search/FeatureInfoPanel.js
@@ -32,8 +32,6 @@ Ext.namespace("Heron.utils");
  *  These allow you to render specific formatting of cell content within the feature grid. For example
  *  URL substitution to render external links in a new tab or browser window. You can even supply your own formatting
  *  function. This function is according to the ExtJS ColumnModel renderers (see e.g. http://snipplr.com/view/40942).
- *  More finegrained grid-control can be obtained with ``gridColumns``. Here the standard ExtJS ``columns``
- *  layout can be specified per feature type. Via ``gridColumns`` should become the preferred method.
  *
  *  .. code-block:: javascript
  *
@@ -49,6 +47,7 @@ Ext.namespace("Heron.utils");
  *			 displayPanels: ['Grid', 'XML'],
  *			 exportFormats: ['CSV', 'XLS'],
  *			 maxFeatures: 10,
+ *           columnCapitalize: false,
  *			 discardStylesForDups: true,
  *			 gridCellRenderers: [
  *						{
@@ -100,39 +99,8 @@ Ext.namespace("Heron.utils");
  *								}
  *							}
  *						}
- *					],
- *				                                       
- * 	      gridColumns: [
- * 	      {
- * 	      featureType: 'states',
- * 	        columns: [
- * 	         {
- *                  header: "State Name",
- *                  width: 120,
- *                  dataIndex: "STATE_NAME"
- *           },
- *           {
- *                  header: "Inhabitants",
- *                  width: 120,
- *                  dataIndex: "PERSONS"
- *           },
- *           {
- *                  header: "More Info",
- *                  width: 120,
- *                  renderer: function (value, metaData, record, rowIndex, colIndex, store) {
- *                      var template = '<a target="_new" href="http://en.wikipedia.org/wiki/{STATE_NAME}">Wikipedia Info</a>';
- *                      var options = {attrNames: ['STATE_NAME']};
- *                      return Heron.widgets.GridCellRenderer.substituteAttrValues(template, options, record);
- *                  }
- *            }
  *
- *           ]
- *         }
- *      ]
- *
- *					
- *
- *    }
+ *		 }
  *
  */
 
@@ -230,18 +198,11 @@ Heron.widgets.search.FeatureInfoPanel = Ext.extend(Ext.Panel, {
      */
     featureSelection: true,
 
-    /** api: config[gridCellRenderers]
-     *  ``Array``
-     *  Have all columns displayed but only these specifically formatted per feature type (see example).
+    /** api: config[columnCapitalize]
+     *  ``Boolean``
+     *  Should the column names be capitalized when autoconfig is true?
      */
-    gridCellRenderers: null,
-
-
-    /** api: config[gridColumns]
-     *  ``Array``
-     *  Have only specific columns displayed and formatted per feature type (see example).
-     */
-    gridColumns: null,
+    columnCapitalize: true,
 
     /** Internal vars */
     pop: null,
@@ -786,27 +747,16 @@ Heron.widgets.search.FeatureInfoPanel = Ext.extend(Ext.Panel, {
                 continue;
             }
 
-            var autoConfig = true;
-            var columns = null;
-            if (this.gridColumns)  {
-                for (var c = 0; c < this.gridColumns.length; c++) {
-                    if (this.gridColumns[c].featureType == featureSet.featureType) {
-                        autoConfig = false;
-                        columns = this.gridColumns[c].columns;
-                        break;
-                    }
-                }
-            }
             var grid = new Heron.widgets.search.FeatureGridPanel({
                 title: featureSet.title,
                 featureType: featureSet.featureType,
                 header: false,
                 features: featureSet.features,
-                autoConfig: autoConfig,
+                autoConfig: true,
+                columnCapitalize: this.columnCapitalize,
                 showGeometries: this.showGeometries,
                 featureSelection: this.featureSelection,
                 gridCellRenderers: this.gridCellRenderers,
-                columns: columns,
                 showTopToolbar: this.showTopToolbar,
                 exportFormats: this.exportFormats,
                 hropts: {
