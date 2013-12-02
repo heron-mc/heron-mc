05203ea75bc2b4b19c61130363d436587ee939ad
Just van den Broecke <just@justobjects.nl>
2013-12-02 21:28:34 +0000

    issue 189 - print example with additional outputs

--- heron/examples/printdialog/Config.js
+++ heron/examples/printdialog/Config.js
@@ -45,66 +45,66 @@ Heron.options.map.toolbar = [
             }
         }
     }},
-	{type: "pan"},
-	{type: "zoomin"},
-	{type: "zoomout"},
-	{type: "zoomvisible"},
-	{type: "-"} ,
-	{type: "printdirect", options: {url: 'http://kademo.nl/print/pdf28992'
-									 , mapTitle: 'My Title - Direct Print'
-									// , mapTitleYAML: "mapTitle"		// MapFish - field name in config.yaml - default is: 'mapTitle'
-									// , mapComment: 'My Comment - Direct Print'
-									// , mapCommentYAML: "mapComment"	// MapFish - field name in config.yaml - default is: 'mapComment'
-									// , mapFooter: 'My Footer - Direct Print'
-									// , mapFooterYAML: "mapFooter"	// MapFish - field name in config.yaml - default is: 'mapFooter'
-									// , mapPrintLayout: "A4"			// MapFish - 'name' entry of the 'layouts' array or Null (=> MapFish default)
-									// , mapPrintDPI: "75"				// MapFish - 'value' entry of the 'dpis' array or Null (=> MapFish default)
-									// , mapPrintLegend: true
-									// , legendDefaults: {
-									//     useScaleParameter : false,
-									//     baseParams: {FORMAT: "image/png"}
-									//   }
-									}},
-	{type: "printdialog", options: {url: 'http://kademo.nl/print/pdf28992', windowWidth: 360
-									// , showTitle: true
-									// , mapTitle: 'My Header - Print Dialog'
-									// , mapTitleYAML: "mapTitle"		// MapFish - field name in config.yaml - default is: 'mapTitle'
-									// , showComment: true
-									// , mapComment: 'My Comment - Print Dialog'
-									// , mapCommentYAML: "mapComment"	// MapFish - field name in config.yaml - default is: 'mapComment'
-									// , showFooter: true
-									// , mapFooter: 'My Footer - Print Dialog'
-									// , mapFooterYAML: "mapFooter"	// MapFish - field name in config.yaml - default is: 'mapFooter'
-									// , showRotation: true
-									// , showLegend: true
-									// , showLegendChecked: true
-									// , mapLimitScales: false
-									}}
+    {type: "pan"},
+    {type: "zoomin"},
+    {type: "zoomout"},
+    {type: "zoomvisible"},
+    {type: "-"} ,
+    {type: "printdirect", options: {url: 'http://kademo.nl/print/pdf28992', mapTitle: 'My Title - Direct Print'
+        // , mapTitleYAML: "mapTitle"		// MapFish - field name in config.yaml - default is: 'mapTitle'
+        // , mapComment: 'My Comment - Direct Print'
+        // , mapCommentYAML: "mapComment"	// MapFish - field name in config.yaml - default is: 'mapComment'
+        // , mapFooter: 'My Footer - Direct Print'
+        // , mapFooterYAML: "mapFooter"	// MapFish - field name in config.yaml - default is: 'mapFooter'
+        // , mapPrintLayout: "A4"			// MapFish - 'name' entry of the 'layouts' array or Null (=> MapFish default)
+        // , mapPrintDPI: "75"				// MapFish - 'value' entry of the 'dpis' array or Null (=> MapFish default)
+        // , mapPrintLegend: true
+        // , legendDefaults: {
+        //     useScaleParameter : false,
+        //     baseParams: {FORMAT: "image/png"}
+        //   }
+    }},
+    {type: "printdialog", options: {url: 'http://kademo.nl/print/pdf28992', windowWidth: 360
+        // , showTitle: true
+        // , mapTitle: 'My Header - Print Dialog'
+        // , mapTitleYAML: "mapTitle"		// MapFish - field name in config.yaml - default is: 'mapTitle'
+        // , showComment: true
+        // , mapComment: 'My Comment - Print Dialog'
+        // , mapCommentYAML: "mapComment"	// MapFish - field name in config.yaml - default is: 'mapComment'
+        // , showFooter: true
+        // , mapFooter: 'My Footer - Print Dialog'
+        // , mapFooterYAML: "mapFooter"	// MapFish - field name in config.yaml - default is: 'mapFooter'
+        // , showRotation: true
+        // , showLegend: true
+        // , showLegendChecked: true
+        // , mapLimitScales: false
+        , showOutputFormats: true
+    }}
 ];
 
 /**
  * Defines the entire layout of a Heron webapp using ExtJS-style.
  **/
 Heron.layout = {
-	xtype: 'panel',
-	id: 'hr-container-main',
-	layout: 'border',
-	border: true,
+    xtype: 'panel',
+    id: 'hr-container-main',
+    layout: 'border',
+    border: true,
 
-	items: [
-		{
-			xtype: 'panel',
+    items: [
+        {
+            xtype: 'panel',
 
-			id: 'hr-menu-left-container',
-			layout: 'accordion',
-			region : "west",
-			width: 240,
-			collapsible: true,
-			split	: false,
-			border: false,
-			items: [
-				{
-					xtype: 'hr_layertreepanel',
+            id: 'hr-menu-left-container',
+            layout: 'accordion',
+            region: "west",
+            width: 240,
+            collapsible: true,
+            split: false,
+            border: false,
+            items: [
+                {
+                    xtype: 'hr_layertreepanel',
                     contextMenu: [
                         {
                             xtype: 'hr_layernodemenulayerinfo'
@@ -117,63 +117,63 @@ Heron.layout = {
                         }
                     ],
 
-					hropts: Heron.options.layertree
-				}
-			]
-		},
-		{
-			xtype: 'panel',
+                    hropts: Heron.options.layertree
+                }
+            ]
+        },
+        {
+            xtype: 'panel',
 
-			id: 'hr-map-and-info-container',
-			layout: 'border',
-			region: 'center',
-			width: '100%',
-			collapsible: false,
-			split	: false,
-			border: false,
-			items: [
-				{
-					xtype: 'hr_mappanel',
-					title: '&nbsp;',
-					id: 'hr-map',
-					region: 'center',
-					collapsible : false,
-					border: false,
-					hropts: Heron.options.map
-				}
-			]
-		},
-		{
-			xtype: 'panel',
+            id: 'hr-map-and-info-container',
+            layout: 'border',
+            region: 'center',
+            width: '100%',
+            collapsible: false,
+            split: false,
+            border: false,
+            items: [
+                {
+                    xtype: 'hr_mappanel',
+                    title: '&nbsp;',
+                    id: 'hr-map',
+                    region: 'center',
+                    collapsible: false,
+                    border: false,
+                    hropts: Heron.options.map
+                }
+            ]
+        },
+        {
+            xtype: 'panel',
 
-			id: 'hr-menu-right-container',
-			layout: 'accordion',
-			region : "east",
-			width: 240,
-			collapsible: true,
-			split	: false,
-			border: false,
-			items: [
-				{
-					xtype: 'hr_layerlegendpanel',
-					id: 'hr-layerlegend-panel',
-					defaults: {
-						useScaleParameter : true,
-						baseParams: {
-							FORMAT: 'image/png'
-						}
-					},
-					hropts: {
-						// Preload Legends on initial startup
-						// Will fire WMS GetLegendGraphic's for WMS Legends
-						// Otherwise Legends will be loaded only when Layer
-						// becomes visible. Default: false
-						prefetchLegends: false
-					}
-				}
-			]
-		}
-	]
+            id: 'hr-menu-right-container',
+            layout: 'accordion',
+            region: "east",
+            width: 240,
+            collapsible: true,
+            split: false,
+            border: false,
+            items: [
+                {
+                    xtype: 'hr_layerlegendpanel',
+                    id: 'hr-layerlegend-panel',
+                    defaults: {
+                        useScaleParameter: true,
+                        baseParams: {
+                            FORMAT: 'image/png'
+                        }
+                    },
+                    hropts: {
+                        // Preload Legends on initial startup
+                        // Will fire WMS GetLegendGraphic's for WMS Legends
+                        // Otherwise Legends will be loaded only when Layer
+                        // becomes visible. Default: false
+                        prefetchLegends: false
+                    }
+                }
+            ]
+        }
+    ]
 };
 
 Ext.namespace("Heron.examples");
@@ -182,98 +182,98 @@ Ext.namespace("Heron.examples");
  * or bound to the "find" button in the toolbar. Here we use the toolbar button.
  */
 Heron.examples.searchPanelConfig = {
-	xtype: 'hr_searchcenterpanel',
-	id: 'hr-searchcenterpanel',
-	title: __('Search'),
-	height: 600,
-	border: false,
-	hropts: {
-		searchPanel: {
-			xtype: 'hr_formsearchpanel',
-			id: 'hr-searchpanel',
-			header: false,
-			border: false,
-			bodyStyle: 'padding: 6px',
-			style: {
-				fontFamily: 'Verdana, Arial, Helvetica, sans-serif',
-				fontSize: '12px'
-			},
-			protocol: new OpenLayers.Protocol.WFS({
-				version: "1.1.0",
-				url: "http://kademo.nl/gs2/wfs?",
-				srsName: "EPSG:28992",
-				featureType: "hockeyclubs",
-				featureNS: "http://innovatie.kadaster.nl"
-			}),
-			items: [
-				{
-					xtype: "textfield",
-					name: "name__like",
-					value: 'hu',
-					fieldLabel: "  name"
-				},
-				{
-					xtype: "label",
-					id: "helplabel",
-					html: 'Type name of an NL hockeyclub, wildcards are appended<br/>Any single letter will also yield results.<br/>',
-					style: {
-						fontSize: '10px',
-						color: '#AAAAAA'
-					}
-				}
-			],
-			hropts: {
-				onSearchCompleteZoom : 10,
-				autoWildCardAttach : true,
+    xtype: 'hr_searchcenterpanel',
+    id: 'hr-searchcenterpanel',
+    title: __('Search'),
+    height: 600,
+    border: false,
+    hropts: {
+        searchPanel: {
+            xtype: 'hr_formsearchpanel',
+            id: 'hr-searchpanel',
+            header: false,
+            border: false,
+            bodyStyle: 'padding: 6px',
+            style: {
+                fontFamily: 'Verdana, Arial, Helvetica, sans-serif',
+                fontSize: '12px'
+            },
+            protocol: new OpenLayers.Protocol.WFS({
+                version: "1.1.0",
+                url: "http://kademo.nl/gs2/wfs?",
+                srsName: "EPSG:28992",
+                featureType: "hockeyclubs",
+                featureNS: "http://innovatie.kadaster.nl"
+            }),
+            items: [
+                {
+                    xtype: "textfield",
+                    name: "name__like",
+                    value: 'hu',
+                    fieldLabel: "  name"
+                },
+                {
+                    xtype: "label",
+                    id: "helplabel",
+                    html: 'Type name of an NL hockeyclub, wildcards are appended<br/>Any single letter will also yield results.<br/>',
+                    style: {
+                        fontSize: '10px',
+                        color: '#AAAAAA'
+                    }
+                }
+            ],
+            hropts: {
+                onSearchCompleteZoom: 10,
+                autoWildCardAttach: true,
                 caseInsensitiveMatch: true,
                 logicalOperator: OpenLayers.Filter.Logical.AND
-			}
-		},
-		resultPanel: {
-			xtype: 'hr_featuregridpanel',
-			id: 'hr-featuregridpanel',
-			title: __('Search'),
-			header: false,
-			border: false,
-			columns: [
-				{
-					header: "Name",
-					width: 100,
-					dataIndex: "name"
-				},
-				{
-					header: "Desc",
-					width: 200,
-					dataIndex: "cmt"
-				}
-			],
-			hropts: {
-				zoomOnRowDoubleClick : true,
-				zoomOnFeatureSelect : false,
-				zoomLevelPointSelect : 8
-			}
-		}
-	}
+            }
+        },
+        resultPanel: {
+            xtype: 'hr_featuregridpanel',
+            id: 'hr-featuregridpanel',
+            title: __('Search'),
+            header: false,
+            border: false,
+            columns: [
+                {
+                    header: "Name",
+                    width: 100,
+                    dataIndex: "name"
+                },
+                {
+                    header: "Desc",
+                    width: 200,
+                    dataIndex: "cmt"
+                }
+            ],
+            hropts: {
+                zoomOnRowDoubleClick: true,
+                zoomOnFeatureSelect: false,
+                zoomLevelPointSelect: 8
+            }
+        }
+    }
 };
 
 Heron.options.map.toolbar.push({type: "-"});
 
 Heron.options.map.toolbar.push(
-		{
-			type: "searchcenter",
-			// Options for SearchPanel window
-			options : {
-				searchWindow : {
-					title: undefined,
-					x: 100,
-					y: undefined,
-					width: 320,
-					height: 400,
-					items: [
-					  Heron.examples.searchPanelConfig
-					]
-				}
-			}
-		}
+        {
+            type: "searchcenter",
+            // Options for SearchPanel window
+            options: {
+                searchWindow: {
+                    title: undefined,
+                    x: 100,
+                    y: undefined,
+                    width: 320,
+                    height: 400,
+                    items: [
+                        Heron.examples.searchPanelConfig
+                    ]
+                }
+            }
+        }
 );
 
