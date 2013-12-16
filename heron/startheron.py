1db73daac91f6fc3596e6ba7e9b6870872cc5ba7
Just van den Broecke <just@justobjects.nl>
2013-12-16 11:47:48 +0000

    issue 321 :  add python webserver .bat file and doc

--- heron/startheron.py
+++ heron/startheron.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
+
+# Python-based webserver to run Heron examples
+#
 import CGIHTTPServer
+import os
 
+# default port and address to listen to
 port = 8000
+address = ''
 
+# Extend as to enable .cgi files to be recognized (Windows issue)
+class MyCGIHTTPRequestHandler(CGIHTTPServer.CGIHTTPRequestHandler):
+    def is_python(self, path):
+        """Test whether argument path is a Python script."""
+        head, tail = os.path.splitext(path)
+        return tail.lower() in (".py", ".pyw", ".cgi")
+
+# Main webserver that also can execute cgi programs
+# NOT FOR PRODUCTION USE!!
 def main():
-    server_address = ('', port)
-    handler = CGIHTTPServer.CGIHTTPRequestHandler
-    # handler.cgi_directories = ['/services/cgi-bin', '/cgi-bin']
-    server = CGIHTTPServer.BaseHTTPServer.HTTPServer(server_address, handler)
+
+    server = CGIHTTPServer.BaseHTTPServer.HTTPServer((address, port), MyCGIHTTPRequestHandler)
     try:
-        print('Heron server starting, browse to http://127.0.0.1:%i, shutdown with Ctrl-C' % port)
+        print('Heron server starting, browse to http://127.0.0.1:%i/examples, shutdown with Ctrl-C' % port)
         server.serve_forever()
     except KeyboardInterrupt:
         server.socket.close()
