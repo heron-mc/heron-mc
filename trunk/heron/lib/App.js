6bd5f0d6d6989e1c89fec3ba4547d56f1876e659
justb4 <justb4@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2014-04-23 15:12:01 +0000

    add Proj4js.defs["EPSG:25831"], somehow gives error in some cases when proj4js tries to load this def from CDN

--- trunk/heron/lib/App.js
+++ trunk/heron/lib/App.js
@@ -27,6 +27,8 @@ Heron.globals = {
 try {
     // Define here for now as this file is always included but we need a better way
     Proj4js.defs["EPSG:28992"] = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.999908 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs";
+    // Somehow Proj4JS tries to load this def, without success...
+    Proj4js.defs["EPSG:25831"] = "+proj=utm +zone=31 +ellps=GRS80 +units=m +no_defs";
 } catch (err) {
     // ignore
 }
