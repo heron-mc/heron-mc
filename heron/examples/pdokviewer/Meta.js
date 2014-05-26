65eeea5f74a31b792da6c08ff02296e48cd2cfea
Just van den Broecke <just@justobjects.nl>
2014-05-26 09:29:25 +0000

    rename example pdokthijs to pdokviewer

--- heron/examples/pdokviewer/Meta.js
+++ heron/examples/pdokviewer/Meta.js
@@ -1,6 +1,6 @@
 // Empty file to generate docs
 
-/** api: example[pdok]
+/** api: example[pdokviewer]
  *  PDOK
  *  ----
  *  Demonstrates Viewer developed for the Dutch National SDI: PDOK (Publieke Dienstverlening Op de Kaart).
