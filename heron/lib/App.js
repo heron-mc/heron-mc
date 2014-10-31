3a8045143a44d7ceb2924820f2fb72f9121a0e34
Just van den Broecke <just@justobjects.nl>
2014-10-31 15:10:46 +0000

    LAST CHECK-IN for v1.0.4

--- heron/lib/App.js
+++ heron/lib/App.js
@@ -20,7 +20,7 @@ Ext.namespace("Heron.globals");
 /** REST Services specific to Heron. */
 Heron.globals = {
     serviceUrl: '/cgi-bin/heron.cgi',
-    version: '1.0.3',
+    version: '1.0.4',
     imagePath: undefined
 };
 
