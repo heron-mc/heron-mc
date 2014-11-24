85b5dfa6846dd1b8af995517a2cf8ad4ec2554fb
martijn.heuff@gmail.com <martijn.heuff@gmail.com@13ca3d86-9c30-f4e7-4096-5b6f3f237a34>
2014-11-24 08:50:35 +0000

    Issue #406: Added support for the Metadata / Legend properties to the HeronMapContext. The HeronMapContext supports now a legendUrl and a hideInLegend for WMS, TMS and WMTS-layers.           <metadata>               <legend>                   <legendUrl>http://www.example.com/legend.png</legendUrl>                   <hideInLegend>true|false</hideInLegend>               </legend>           </metadata>

--- trunk/heron/examples/legendcustom/Config.js
+++ trunk/heron/examples/legendcustom/Config.js
@@ -12,13 +12,24 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 Ext.namespace("Heron");
+
+
+Ext.namespace("Heron.options.map");
+Heron.options.map.settings = {
+    projection: 'EPSG:4326',
+    units: 'm',
+//    maxExtent: '-104929,-56303,128235,113521',
+    center: [-95.8506355, 37.163851],
+    zoom: 15
+};
 
 
 /** api: example[legendcustom]
  *  Custom Legend
  *  -------------
- *  Demonstrates how to configure a custom legend image as URL for a Layer.
+ *  Demonstrates how to configure a custom legend image as URL for a Layer or hide the standard legend of the Layer.
  */
 
 /**
@@ -41,6 +52,13 @@ Heron.layout = {
 
             /** Below are Heron-specific settings for the MapPanel (xtype: 'hr_mappanel') */
             hropts: {
+                settings: {
+                    projection: 'EPSG:4326',
+                    restrictedExtent: [-180, 10, -60, 80],
+//                    maxExtent : [-180, 5, 0, 90],
+                    center: [-120, 35],
+                    zoom: 3
+                },
                 layers: [
                     // Using the new config method without "new"
                     ["OpenLayers.Layer.WMS", "World Map",
@@ -48,15 +66,76 @@ Heron.layout = {
                         {layers: 'Coastlines', format: 'image/png' },
                         { metadata: {
                             legend: {
-                                // Use a fixed URL as legend
-                                legendURL: 'legenda-t25raster.png',
-                                hideInLegend: false
+                                // Hide the legend for this layer
+                                hideInLegend: true
                             }
                         }
                         }
-                    ]
+                    ],
+
+                    new OpenLayers.Layer.WMTS({
+                        name: "Earth (WMTS) ",
+                        url: "http://demo.opengeo.org/geoserver/gwc/service/wmts?",
+                        layer: "ne:NE1_HR_LC_SR_W_DR",
+                        matrixSet: "EPSG:4326",
+                        matrixIds: ["EPSG:4326:0", "EPSG:4326:1", "EPSG:4326:2", "EPSG:4326:3", "EPSG:4326:4", "EPSG:4326:5", "EPSG:4326:6", "EPSG:4326:7", "EPSG:4326:8", "EPSG:4326:9", "EPSG:4326:10", "EPSG:4326:11", "EPSG:4326:12", "EPSG:4326:13", "EPSG:4326:14", "EPSG:4326:15", "EPSG:4326:16", "EPSG:4326:17", "EPSG:4326:18", "EPSG:4326:19", "EPSG:4326:20", "EPSG:4326:21"],
+                        format: "image/png",
+                        visibility: true,
+                        style: "_null",
+                        opacity: 0.7,
+                        isBaseLayer: false,
+                        metadata: {
+                            legend: {
+                                // Use a fixed URL as legend
+                                legendURL: 'earth.png',
+                                hideInLegend: false
+                            }
+                        }
+                    }),
+
+                    ["OpenLayers.Layer.WMS", "States",
+                        "http://demo.opengeo.org/geoserver/ows?",
+                        {
+                            layers: 'topp:states',
+                            format: 'image/png',
+                            transparent: true
+                        },
+                        {
+                            isBaseLayer: false,
+                            visibility: true,
+                            metadata: {
+                                legend: {
+                                    // Use a fixed URL as legend
+                                    legendURL: 'statespopulationlegend.png',
+                                    hideInLegend: false
+                                }
+                            }
+                        }
+                        ],
+
+                    new OpenLayers.Layer.WMS(
+                        "10m Places",
+                        "http://demo.opengeo.org/geoserver/ows?",
+                        {
+                            layers: 'osm:ne_10m_populated_places',
+                            format: 'image/png',
+                            transparent: true
+                        },
+                        {
+                            isBaseLayer: false,
+                            visibility: true,
+                            metadata: {
+                                legend: {
+                                    // Use a fixed URL as legend
+                                    legendURL: 'legendCities.png',
+                                    hideInLegend: false
+                                }
+                            }
+                        }
+                    )
                 ]
-            }},
+            }
+        },
         {
             xtype: 'hr_layerlegendpanel',
             region: "east",
