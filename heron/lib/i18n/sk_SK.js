378a9e565acc41ef48d8163f940a087ef121c00a
Just van den Broecke <just@justobjects.nl>
2015-05-27 13:17:45 +0200

    issue #423 - slovak localization (translation to SLOVAK language) - sk_SK.js

---
+++ heron/lib/i18n/sk_SK.js
@@ -0,0 +1,265 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+Ext.namespace("Heron.i18n");
+
+/** api: (define)
+ *  module = Heron.i18n
+ *  class = Heron.i18n.dict (sk_SK)
+ *  base_link = `Ext.form.ComboBox <http://dev.sencha.com/deploy/ext-3.3.1/docs/?class=Ext.form.ComboBox>`_
+ */
+
+/**
+ * Define dictionary for the SK locale.
+ * Maintained by: Stanislav Motyčka and Heron devs
+ */
+
+Heron.i18n.dict = {
+
+	// 0.67
+	'Active Layers': 'Aktívne­ vrstvy',
+	'Base Layer': 'Základná­ vrstva',
+	'Base Layers': 'Základné vrstvy',
+	'BaseMaps': 'Základné­ mapy',
+	'Choose a Base Layer': 'Vyberte základnú­ vrstvu',
+	'Legend': 'Legenda',
+	'Feature Info': 'Informácie o prvku',
+	'Feature Data': 'Údaje prvku',
+	'Feature(s)': 'Prvky',
+	'No layer selected': 'Nebola vybratá žiadna vrstva',
+	'Save Features': 'Uložiť prvky',
+	'Get Features': 'Zí­skať prvky',
+	'Feature information': 'Informácie o prvku',
+	'No Information found': 'Nenájdené žiadne informácie',
+	'Layer not added': 'Vrstva nebola pridaná',
+	'Attribute': 'Atribút',
+	'Value': 'Hodnota',
+	'Recieving data': 'Prijímám údaje',
+	'Layers': 'Vrstvy',
+	'No match': 'Žiadna zhoda',
+	'Loading...': 'Načítáné­...',
+	'Bookmarks': 'Záložky',
+	'Places': 'Mie­sta',
+	'Unknown': 'Neznáme˝',
+	'Feature Info unavailable': 'Informácie o prvku nie sú k dispozícii',
+	'Pan': '<b>Posun</b><br>Držanie­ ľavého tlačidla myši nad mapou posúva aktuálny­<br>pohľad; koliesko myši a pozícia kurzoru nad mapou<br>zároveň ovláda priblíženie (aj v iných režimoch ako Posun)',
+	'Measure length': '<b>Merané dĺžky</b><br>Každé kliknutie­ na ľavé tlačidlo myši nad mapou vytvorí­,<br>bod meraného úseku, dvojité kliknutie­ meranie­ ukončí',
+	'Measure area': '<b>M??ené­ plochy</b><br>Každé kliknutie­ na ľavé tlačidlo myši nad mapou vytvorí­,<br>bod meraného polygónu, dvojité kliknutie­ meranie­ ukončí­',
+	'Leg': 'Úsek',
+	'Length': 'Dĺžka',
+	'Area': 'Plocha',
+	'Result >': 'Výsledok >',
+	'< Search': '< Hľadať',
+	'Search': 'Hľadať',
+	'Search Nominatim': 'Hľadať (pomocou OSM Nominatim) podľa názvu a adresy',
+	'Search OpenLS': 'Hľadať pomocou služby OpenLS',
+	'Search PDOK': 'Hľadať PDOK',
+	'Searching...': 'Hľadanie...',
+	'Search Completed: ': 'Hľadanie dokončené: ',
+	'services': 'služby',
+	'service': 'služba',
+	'Type Nominatim': 'Zadajte názov miesta alebo adresu...',
+	'Overlays': 'Prekryvné vrstvy',
+	'Waiting for': 'Čakám na',
+	'Warning': 'Varovanie',
+	'Zoom in': '<b>Priblížiť</b><br>Kliknutím na ľavé tlačidlo myši sa na mape priblíží pohľad,<br>držaním tlačidla a ťahaním je možné označiť plochu pre priblíženie',
+	'Zoom out': '<b>Oddialiť</b><br>Kliknutím na ľavé tlačidlo myši sa na mape oddiali pohľad,<br>držaním tlačidla a ťahaním je možné označiť plochu pre oddialenie',
+	'Zoom to full extent': '<b>Oddialiť na plný rozsah</b><br>Oddiali pohľad pre plné zobrazenie povoleného rozsahu mapy',
+	'Zoom previous': '<b>Predchádzajúci pohľad</b><br>Prejde na predchádzajúci pohľad (zväčšenie aj rozsah)',
+	'Zoom next': '<b>Ďalší pohľad</b><br>Prejde na ďalší pohľad (zväčšenie aj rozsah)',
+	'Zoom': 'Priblíženie',
+
+	// 0.68
+	'Scale': 'Mierka',
+	'Resolution': 'Rozlíšenie',
+	'Zoom': 'Priblíženie',
+	'Create PDF': 'Vytvoriť PDF',
+	'Print': 'Tlač',
+	'Print Dialog Popup': 'Dialogové okno tlače',
+	'Print Visible Map Area': 'Tlač viditeľnej plochy mapy',
+
+	// 0.70
+	'Export': 'Exportovať',
+	'Choose a Display Option': 'Vyberte si možnosť zobrazenia',
+	'Display': 'Zobrazenie',
+	'Grid': 'Tabuľka',
+	'Tree': 'Strom',
+	'XML': 'XML',
+	'Invalid export format configured: ': 'Nastavený neplatný formát exportu: ',
+	'No features available or none-grid display chosen': 'Nie sú k dispozícii žiadne prvky alebo nebolo vybrané zobrazenie Tabuľka',
+	'Choose an Export Format': 'Vyberte si formát pre export',
+	'Print Visible Map Area Directly': 'Priama tlač viditelnej plochy mapy',
+	'Direct Print Demo': 'Ukážka priamej tlače',
+	'This is a simple map directly printed.': 'Toto je priamo vytlačená jednoduchá mapa.',
+	'Print Dialog Popup with Preview Map': '<b>Tlač</b><br>Otvorí okno s náhľadom a nastavením tlače',
+	'Print Preview': 'Náhľad tlače',
+	'Print Preview Demo': 'Ukážka náhľadu tlače',
+	'Error getting Print options from server: ': 'Chyba pri získávaní nastavenia tlače ze servera: ',
+	'Error from Print server: ': 'Chyba tlačového servera: ',
+	'No print provider url property passed in hropts.': 'V hropts nie je nadefinovaná URL adresa tlačového servera.',
+	'Create PDF...': 'Vytvoriť PDF...',
+	'Loading print data...': 'Nahrávanie údajov tlače...',
+
+	// 0.71
+	'Go to coordinates': 'Prejsť na súradnice',
+	'Go!': 'Prejsť!',
+	'Pan and zoom to location': 'Posunúť a priblížiť na pozíciu',
+	'Enter coordinates to go to location on map': 'Vložte súradnice pre prechod na pozíciu na mape',
+	'Active Themes': 'Aktívne vrstvy',
+	'Move up': 'Posunúť hore',
+	'Move down': 'Posunúť dole',
+	'Opacity': 'Priehľadnosť',
+	'Remove layer from list': 'Odobrať vrstvu zo zoznamu',
+	'Tools': 'Nástroje',
+	'Removing': 'Odoberám',
+	'Are you sure you want to remove the layer from your list of layers?': 'Ste si istý, že chcete odstrániť vrstvu z Vášho zoznamu vrstiev?',
+	'You are not allowed to remove the baselayer from your list of layers!': 'Nemáte dovolené odstrániť základnú vrstvu z Vášho zoznamu vrstiev!',
+
+	// 0.72
+	'Draw Features': 'Kresliť prvky',
+
+	// 0.73
+	'Spatial Search': 'Priestorové vyhľadávanie',
+	'Search by Drawing': 'Hľadanie podľa kresby',
+	'Select the Layer to query': 'Vyberte vrstvu na požiadavku',
+	'Choose a geometry tool and draw with it to search for objects that touch it.': 'Vyberte nástroj pre vytvorenie geometrie a použite ju pre hľadanie objektov, ktoré sa jej dotýkajú.',
+	'Seconds': 'Sekundy',
+	'Working on it...': 'Pracuje sa na tom...',
+	'Still searching, please be patient...': 'Stále hľadám, prosím o chvíľku strpenia...',
+	'Still searching, have you selected an area with too many objects?': 'Stále hľadám, nevybrali ste oblasť s príliš mnohými objektami?',
+	'as': 'ako',
+	'Undefined (check your config)': 'Nedefinované (skontrolujte konfiguráciu)',
+	'Objects': 'Objekty',
+	'objects': 'objekty',
+	'Features': 'Prvky',
+	'features': 'prvky',
+	'Result': 'Výsledok',
+	'Results': 'Výsledky',
+	'of': 'od',
+	'Using geometries from the result: ': 'Pomocou geometrií z výsledku: ',
+	'with': 's',
+	'Too many geometries for spatial filter: ': 'Príliš mnoho geometrií pre priestorový filter: ',
+	'Bookmark current map context (layers, zoom, extent)': '<b>Vytvoriť záložku</b><br>Vytvorí záložku aktuálnej mapy (vrstvy, priblíženie, rozsah)',
+	'Add a bookmark': 'Vytvoriť záložku',
+	'Bookmark name cannot be empty': 'názov záložky nesmie byť prázdny',
+	'Your browser does not support local storage for user-defined bookmarks': 'Váš prehliadač neumožňuje lokálne úložisko pre užívateľsky definované záložky',
+	'Return to map navigation': 'Späť na navigáciu v mape',
+	'Draw point': 'Kresliť bod',
+	'Draw line': 'Kresliť líniu',
+	'Draw polygón': 'Kresliť polygón',
+	'Draw circle (click and drag)': 'Kresliť kruh (kliknutím a ťahaním)',
+	'Draw Rectangle (click and drag)': 'Kresliť pravouhoľník (kliknutím a ťahaním)',
+	'Sketch is saved for use in Search by Selected Features': 'Skica bola uložená pre použitie pri vyhľadávaní podľa vybraných prvkov',
+	'Select a search...': 'Vybrať vyhľadávanie...',
+	'Clear': 'Vyčistiť',
+
+	// 0.74
+	'Project bookmarks': 'Záložky projektu',
+	'Your bookmarks': 'Vaše záložky',
+	'Name': 'Meno',
+	'Description': 'Popis',
+	'Add': 'Pridať',
+	'Cancel': 'Zrušiť',
+	'Remove bookmark:': 'Odstrániť záložku:',
+	'Restore map context:': 'Obnoviť kontext mapy:',
+	'Error: No \'BookmarksPanel\' found.': 'Chyba: nenájdený \'BookmarksPanel\'.',
+	'Input system': 'Súradnicový systém',
+	'Choose input system...': 'Vyberte súradnicový systém...',
+	'Map system': 'Súradnicový systém',
+	'X': 'X',
+	'Y': 'Y',
+	'Enter X-coordinate...': 'Vložte koordinát X...',
+	'Enter Y-coordinate...': 'Vložte koordinát Y...',
+	'Choose scale...': 'Vyberte mierku...',
+	'no zoom': 'žiadne priblíženie',
+	'Mode': 'Režim',
+	'Remember locations': 'Pamätať si miesta',
+	'Hide markers on close': 'Pri zatvorení skryť značky',
+	'Remove markers on close': 'Pri zatvorení odstrániť značky',
+	'Remove markers': 'Odstrániť značky',
+	'Location': 'Umiestnenie',
+	'Marker position: ': 'Pozícia značky: ',
+	'No features found': 'Nenájdené žiadne prvky',
+	'Feature Info unavailable (you may need to make some layers visible)': 'Informácie o prvkoch nie sú k dispozícii (možno budete musieť zviditelniť niektoré vrstvy)',
+	'Search by Feature Selection': 'Vyhľadávanie výberom prvku',
+	'Download': 'Stiahnutie',
+	'Choose a Download Format': 'Vyberte formát súboru',
+	'Remove all results': 'Odobrať všetky výsledky',
+	'Download URL string too long (max 2048 chars): ': 'URL reťazec na stiahnutie je príliš dlhý (max. 2048 znakov): ',
+
+	// 0.75
+	'Query Panel': 'Panel dopytov',
+	'Cancel current search': 'Zrušiť aktuálne vyhľadávanie ',
+	'Search in target layer using the selected filters': 'Vyhľadať v cieľovej vrstve použitím vybraných filtrov',
+	'Ready': 'Pripravený',
+	'Search Failed': 'Vyhľadávanie zlyhalo',
+	'Search aborted': 'Vyhľadávanie zrušené',
+
+	// 0.76
+	'No query layers found': 'Žiadne vrstvy dopytu nenájdené',
+	'Edit Layer Style': 'Upraviť štýl vrstvy',
+	'Zoom to Layer Extent': 'Priblížiť na rozsah vrstvy',
+	'Get Layer information': 'Získať informácie o vrstve',
+	'Change Layer opacity': 'Zmeniť priehľadnosť vrstvy',
+	'Select a drawing tool and draw to search immediately': 'Vyberte kresliaci nástroj a nakreslite objekt pre okamžité hľadanie',
+	'Search in': 'Hľadať v',
+	'Search Canceled': 'Vyhľadávanie zrušené',
+	'Help and info for this example': 'Nápoveda a informácie pre tento príklad',
+
+	// 1.0.1
+	'Details': 'Detaily',
+	'Table': 'Tabuľka',
+	'Show record(s) in a table grid': 'Zobraz výsledok(-ky) v tabuľkovom gride',
+	'Show single record': 'Zobraz samostatný záznam',
+	'Show next record': 'Zobraz ďalší záznam',
+	'Show previous record': 'Zobraz predchádzajúci záznam',
+	'Feature tooltips' : 'Feature tooltips',
+	'FeatureTooltip' : 'FeatureTooltip',
+	'Upload features from local file' : 'Nahrať prvky z lokálneho súboru',
+	'My Upload' : 'Moje nahratie',
+	'Anything is allowed here' : 'Všetko je tu povolené',
+	'Edit vector Layer styles' : 'Úprava stýlov vektorovej vrstvy',
+	'Style Editor' : 'Editor štýlov',
+	'Open a map context (layers, styling, extent) from file' : 'Otvoriť kontext mapy (vrstvy, štýly, rozsah) zo súboru',
+	'Save current map context (layers, styling, extent) to file' : 'Uložiť zvolený kontext mapy (vrstvy, štýly, rozsah) do súboru',
+	'Upload' : 'Nahrať',
+	'Uploading file...' : 'Nahrávanie súboru...',
+	'Change feature styles': 'Zmeniť štýl prvku',
+	'Error reading map file, map has not been loaded.': 'Chyba načítania mapového súboru, mapa nebola načítaná.',
+	'Error on removing layers.': 'Chyba pri odstraňovaní vrstiev.',
+	'Error loading map file.': 'Chyba pri načítaní mapového súboru.',
+	'Error reading layer tree.': 'Chyba pri načítaní stromu vrstiev.',
+
+	// 1.0.2
+	'No file specified.': 'Žiaden súbor nebol zvolený.',
+	'Cannot render draw controls': 'Nie je možné zobraziť kresliace nástroje',
+	'Warning - Line Length is ': 'Upozornenie - dĺžka línie je ',
+	'You drew a line with length above the layer-maximum of ': 'Nakreslili ste líniu s dĺžkou viac ako maximum danej vrstvy ',
+	'Warning - Area is ': 'Upozornenie - Plocha je ',
+	'You selected an area for this layer above its maximum of ': 'Pre túto vrstvu ste vybrali plochu väčšiu ako maximum danej vrstvy ',
+	'Error creating Layer': 'Chyba vytvorenia vrstvy',
+	'Error in ajax request': 'Chyba v ajax požiadavke',
+	'Add layers': 'Pridať vrstvy',
+	'Remove layer': 'Odstrániť vrstvu',
+	'Loading map context from:': 'Načítanie kontextu mapy z:',
+
+	// 1.0.3 (nothing added)
+
+ 	// 1.0.4
+	'INTERSECTS (default)': 'PRETÍNA (predvolené)',
+	'WITHIN': 'V RÁMCI',
+	'WITHIN DISTANCE': 'V RÁMCI VZDIALENOSTI',
+	'CONTAINS': 'OBSAHUJE',
+	'Simple Time Slider': 'jednoduchý časový bežec'
+};
